bc5193774d444e7db711600f2c59a6901e18f76a
MAGALA-RICHARD <rmagala640@gmail.com>
2023-10-05 16:37:05 -0500

    new files pool

--- apsimNGpy/parallel/process.py
+++ apsimNGpy/parallel/process.py
@@ -1,6 +1,7 @@
 from concurrent.futures import ProcessPoolExecutor, as_completed,  ThreadPoolExecutor
 import glob, os, sys
 from time import perf_counter
+from tqdm import tqdm
 from os.path import dirname
 from os.path import join as opj
 path = dirname(os.path.realpath(__file__))
@@ -9,6 +10,7 @@ path = opj(root, 'utililies')
 sys.path.append(path)
 from run_utils import run_model
 import utils
+
 
 #_______________________________________________________________
 def  run_apsimxfiles_in_parallel(files, ncores, use_threads=False):
@@ -19,12 +21,23 @@ def  run_apsimxfiles_in_parallel(files, ncores, use_threads=False):
     """
     if not use_threads:
         a = perf_counter()
-        with ProcessPoolExecutor( ncores) as pool:
-               ap= pool.map(run_model, path)
-        print(perf_counter() - a, 'seconds', f'to run {len(files)}')
+        with ProcessPoolExecutor(ncores) as pool:
+           futures  =  [pool.submit(run_model, i) for i in files]
+           progress = tqdm(total=len(futures), position=0, leave=True, bar_format='{percentage:3.0f}% completed')
+           # Iterate over the futures as they complete
+           for future in as_completed(futures):
+               future.result()  # retrieve the result (or use it if needed)
+               progress.update(1)
+           progress.close()
+        print(perf_counter() - a, 'seconds', f'to run {len(files)} files')
     else:
         a = perf_counter()
-        with ThreadPoolExecutor(ncores) as pool:
-            ap = pool.map(run_model, path)
-        print(perf_counter() - a, 'seconds', f'to run {len(files)}')
-
+        with ThreadPoolExecutor(ncores) as tpool:
+            futures = [tpool.submit(run_model, i) for i in files]
+            progress = tqdm(total=len(futures), position=0, leave=True, bar_format='{percentage:3.0f}% completed')
+            # Iterate over the futures as they complete
+            for future in as_completed(futures):
+                future.result()  # retrieve the result (or use it if needed)
+                progress.update(1)
+            progress.close()
+        print(perf_counter() - a, 'seconds', f'to run {len(files)} files')

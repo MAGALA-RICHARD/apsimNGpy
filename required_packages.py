a2c6b739005658a07b0dd02af97272a227dda0c7
MAGALA-RICHARD <rmagala@iastate.edu>
2023-10-05 19:19:11 -0500

    required

--- required_packages.py
+++ required_packages.py
@@ -1,37 +1,9 @@
-from setuptools import setup, find_packages
 from subprocess import check_call
 from pkgutil import find_loader
 import sys
 import importlib
 import os
 import pkgutil
-VERSION = '0.0.1dev'
-DESCRIPTION = 'apsimx next generation package interface'
-LONG_DESCRIPTION = 'run, edit, download soils and weather and interact with the apsimx file'
-setup(
-    name='apsimNGpy',
-    version='1.0.0',
-    url='https://github.com/MAGALA-RICHARD/apsimNGpy.git',
-    license='MIT',
-    author='Richard Magala',
-    author_email='magalarich20@gmail.com',
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    include_package_data=True,
-
-     keywords=['python', 'apsim'],
-     classifiers= [
-            "Development Status :: trial",
-            "Intended Audience :: Education",
-            "Programming Language :: Python :: 3",
-            "Operating System :: Microsoft :: Windows",
-        ],
-     install_requires=[
-        'numpy>=1.20.0',
-        'scipy>=1.7'
-    ]
-)
 
 def check_if_package_Loaded(package):
   load_package  = find_loader(package)

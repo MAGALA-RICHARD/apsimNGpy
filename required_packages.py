58981b410e9bf4ba4dd653b41382d5d8770933ed
MAGALA-RICHARD <rmagala@iastate.edu>
2023-10-05 19:25:33 -0500

    tru

--- required_packages.py
+++ required_packages.py
@@ -8,20 +8,19 @@ import pkgutil
 def check_if_package_Loaded(package):
   load_package  = find_loader(package)
   if load_package != None:
-    value = True
-    return value
+    return True
 # check if the package is loaded, then install it
-for pkg in ['xmltodict', 'urllib', 'scipy', 'pandas', 'numpy', 'pythonnet', 'clr', 'geopandas', 'matplotlib',
-            'requests', 'tqdm', 'progressbar', 'numpy', 'shapely']:
-  if check_if_package_Loaded(pkg) !=True:
-    check_call([sys.executable, '-m', 'pip', 'install', pkg])
-    print(f'{pkg} was installed successfully')
-    # import it again
-    try:
-        globals()[pkg] = importlib.import_module(pkg)
-        # print the success
-        print(f' confirmed {pkg} has been imported successfully')
-    except ImportError as e:
-       print(e)
-       print(f"package{pkg} installation failed")
-
+def run_installation():
+    for pkg in ['xmltodict', 'urllib', 'scipy', 'pandas', 'numpy', 'pythonnet', 'clr', 'geopandas', 'matplotlib',
+                'requests', 'tqdm', 'progressbar', 'numpy', 'shapely']:
+      if check_if_package_Loaded(pkg) !=True:
+        check_call([sys.executable, '-m', 'pip', 'install', pkg])
+        print(f'{pkg} was installed successfully')
+        # import it again
+        try:
+            globals()[pkg] = importlib.import_module(pkg)
+            # print the success
+            print(f' confirmed {pkg} has been imported successfully')
+        except ImportError as e:
+           print(e)
+           print(f"package{pkg} installation failed")

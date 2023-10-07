381401431f4068072950981be715102c3bfb7bd8
MAGALA-RICHARD <rmagala640@gmail.com>
2023-10-07 00:10:12 -0500

    includes data changes

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     include_package_data=True,
-    package_dir={"apsimNGpy"},
+    package_data={'': ['basefiles/*.apsimx']},
      keywords=['python', 'apsim'],
      classifiers= [
             "Development Status :: trial",

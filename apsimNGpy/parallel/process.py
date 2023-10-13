761dcb52635c778baac381ac76993bc988cb7d77
MAGALA-RICHARD <rmagala640@gmail.com>
2023-10-12 20:50:07 -0500

    added read_multiple databases

--- apsimNGpy/parallel/process.py
+++ apsimNGpy/parallel/process.py
@@ -8,7 +8,7 @@ path = dirname(os.path.realpath(__file__))
 root  = dirname(path)
 path = opj(root, 'utililies')
 sys.path.append(path)
-from run_utils import run_model
+from run_utils import run_model, read_simulation
 
 
 #_______________________________________________________________
@@ -35,6 +35,38 @@ def  run_apsimxfiles_in_parallel(iterable_files, ncores, use_threads=False):
         a = perf_counter()
         with ThreadPoolExecutor(ncores) as tpool:
             futures = [tpool.submit(run_model, i) for i in files]
+            progress = tqdm(total=len(futures), position=0, leave=True, bar_format='{percentage:3.0f}% completed')
+            # Iterate over the futures as they complete
+            for future in as_completed(futures):
+                future.result()  # retrieve the result (or use it if needed)
+                progress.update(1)
+            progress.close()
+        print(perf_counter() - a, 'seconds', f'to run {len(files)} files')
+
+
+def read_result_in_parallel(iterable_files, ncores, use_threads=False):
+    """
+    files: lists of apsimx simulation files
+    ncores  =no of cores or threads to use (integer)
+    use_thread: if true thread pool executor will be used if false processpool excutor will be called (boolean)
+    """
+    # remove duplicates. because duplicates will be susceptible to race conditioning in paralell computing
+    files = set(iterable_files)
+    if not use_threads:
+        a = perf_counter()
+        with ProcessPoolExecutor(ncores) as pool:
+            futures = [pool.submit(read_simulation, i) for i in files]
+            progress = tqdm(total=len(futures), position=0, leave=True, bar_format='{percentage:3.0f}% completed')
+            # Iterate over the futures as they complete
+            for future in as_completed(futures):
+                future.result()  # retrieve the result (or use it if needed)
+                progress.update(1)
+            progress.close()
+        print(perf_counter() - a, 'seconds', f'to run {len(files)} files')
+    else:
+        a = perf_counter()
+        with ThreadPoolExecutor(ncores) as tpool:
+            futures = [tpool.submit(read_simulation, i) for i in files]
             progress = tqdm(total=len(futures), position=0, leave=True, bar_format='{percentage:3.0f}% completed')
             # Iterate over the futures as they complete
             for future in as_completed(futures):
--- apsimNGpy/utililies/run_utils.py
+++ apsimNGpy/utililies/run_utils.py
@@ -145,6 +145,41 @@ def run_model(path):
         print(f"apsimNGpy had issues running file {path} : because of {repr(e)}")
         pass
 
+def read_simulation(datastore, report_name= 'MaizeR'):
+    ''' returns all data frame the available report tables'''
+    conn = sqlite3.connect(datastore)
+    cursor = conn.cursor()
+    # reading all table names
+    table_names = [a for a in cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table'")]
+    table_list = []
+    for i in table_names:
+        table_list.append(i[0])
+        # remove these
+    rm = ['_InitialConditions', '_Messages', '_Checkpoints', '_Units']
+    for i in rm:
+        if i in table_list:
+            table_list.remove(i)
+            # start selecting tables
+    select_template = 'SELECT * FROM {table_list}'
 
+    # create data fram dictionary to keep all the tables
+    dataframe_dict = {}
+
+    for tname in table_list:
+        query = select_template.format(table_list=tname)
+        dataframe_dict[tname] = pd.read_sql(query, conn)
+    # close the connection cursor
+    conn.close()
+    dfl = len(dataframe_dict)
+    if len(dataframe_dict) == 0:
+        print("the data dictionary is empty. no data has been returned")
+        # else:
+        # remove elements
+        # print(f"{dfl} data frames has been returned")
+
+    if report_name:
+        return dataframe_dict[report_name]
+    else:
+        return dataframe_dict
 
 

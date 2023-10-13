6998cb69023f30b5b230ca9198094ff5c8dfc0e3
MAGALA-RICHARD <rmagala640@gmail.com>
2023-10-12 21:27:38 -0500

    added read_multiple databases

--- apsimNGpy/apsimpy.py
+++ apsimNGpy/apsimpy.py
@@ -781,12 +781,12 @@ class APSIMNG():
             clock = sim.FindChild[Models.Clock]()
             
             if start_date is not None:
-                dateString1 = f'{start_date} +"T00:00:00"'
+                dateString1 = f'{start_date} T00:00:00'
                 self.start = DateTime.Parse(dateString1) 
                 clock.Start = dateString1
                                
             if end_date is not None:
-                dateString2 = f'{end_date} + "T00:00:00"'
+                dateString2 = f'{end_date} T00:00:00'
                 self.end = DateTime.Parse(dateString2)  
                 clock.End = dateString2
          

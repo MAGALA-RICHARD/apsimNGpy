5b1130259038286591e3ed71c3556d2972afb622
MAGALA-RICHARD <rmagala640@gmail.com>
2023-10-23 16:51:48 -0500

    testing relative import

--- apsimNGpy/utililies/__init__.py
+++ apsimNGpy/utililies/__init__.py
@@ -1,4 +1,3 @@
-from os.path import dirname, basename, isfile, join
-import glob
-modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+from .utils import collect_runfiles, load_from_numpy, upload_apsimx_file_by_pattern, assign_management_practices, make_apsimx_clones,\
+    organize_crop_rotations, evaluate_crop
+__all__ = ['load_from_numpy', 'collect_runfiles']

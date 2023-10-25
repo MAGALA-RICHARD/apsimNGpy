851aaa57f5cd1e9a28cbd3d8753605562938a17d
MAGALA-RICHARD <rmagala@iastate.edu>
2023-10-25 14:42:50 -0500

    readme

--- apsimNGpy/add.py
+++
@@ -1,7 +0,0 @@
-import os
-from importlib.resources import files
-# from utililies.utils import collect_runfiles
-from .utililies.utils import collect_runfiles
-wp = 'NewMetrrr.met'
-def add(a, b):
-    return a +b

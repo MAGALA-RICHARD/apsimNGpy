5f1790d31658e441bcf01141cb7879692e1d9a16
MAGALA-RICHARD <rmagala640@gmail.com>
2023-10-24 19:19:24 -0500

    change main init file

--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -2,5 +2,6 @@ from os.path import dirname, basename, isfile, join
 import glob
 from weather import daymet_bylocation_nocsv, daymet_bylocation, daterange
 from base_data import load_example_files
+import apsimNGpy.manager.soilmanager as soil_manager
 
-__all__ = [daymet_bylocation_nocsv, daymet_bylocation, daterange, load_example_files]
+__all__ = [daymet_bylocation_nocsv, daymet_bylocation, daterange, load_example_files, soil_manager]

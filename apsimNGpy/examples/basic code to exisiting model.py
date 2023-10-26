afb73c1e6e72785d08f652f7b91b79cead7564eb
MAGALA-RICHARD <rmagala640@gmail.com>
2023-10-25 22:53:58 -0500

    basic example

--- apsimNGpy/examples/basic code to exisiting model.py
+++ apsimNGpy/examples/basic code to exisiting model.py
@@ -2,7 +2,7 @@ from apsimNGpy.weather import daymet_bylocation_nocsv
 from apsimNGpy.base_data import load_example_files
 from apsimNGpy.model.soilmodel import SoilModel
 from pathlib import Path
-# this will be the path to your user profile mine is C:\Users\rmagala. feel free to change
+# this will be the path to your user profile. Mine is C:\Users\rmagala. feel free to change
 cwd = Path.cwd().home()
 # get sample apsimx file
 data = load_example_files(cwd)
@@ -12,3 +12,5 @@ apsim = SoilModel(maize)
 # run the model
 apsim.run_edited_file()
 # collect the results from the model
+res = apsim.results
+print(res)

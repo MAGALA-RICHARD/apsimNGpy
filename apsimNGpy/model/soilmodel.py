1ca5de45de1ec69412a56ee2cf757c12cd5393d1
MAGALA-RICHARD <rmagala640@gmail.com>
2023-10-26 14:34:50 -0500

    no property

--- apsimNGpy/model/soilmodel.py
+++ apsimNGpy/model/soilmodel.py
@@ -398,7 +398,3 @@ class SoilModel(APSIMNG):
             print(data_run[0].ToString())
         self.results = self._read_simulation()  # still wondering if this should be a static method
         return self.results
-
-from apsimNGpy.base_data import load_example_files
-dat = load_example_files(r'C:')
-ap = SoilModel(dat.get_maize)

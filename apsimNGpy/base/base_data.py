234faa624dd5f2852921ddc01534ee408701f8d1
MAGALA-RICHARD <rmagala@iastate.edu>
2023-10-26 14:02:58 -0500

    base

---
+++ apsimNGpy/base/base_data.py
@@ -0,0 +1,177 @@
+import os.path
+from importlib.resources import files
+from os.path import join, realpath, dirname, exists, split, basename
+from os import listdir, walk, getcwd, mkdir
+from apsimNGpy.model.soilmodel import SoilModel
+import shutil
+from pathlib import Path
+from functools import cache
+
+wp = 'NewMetrrr.met'
+
+
+def _weather(path):
+    resource_directory = files('apsimNGpy')
+    data_file_path = resource_directory / 'basefiles' / wp
+    nameout = join(path, wp)
+    contents = data_file_path.read_text()
+    with open(nameout, "w+") as openfile:
+        openfile.write(contents)
+    return nameout
+
+
+def _get_maize_example(file_path):
+    resource_directory = files('apsimNGpy')
+    json_file_path = resource_directory / 'basefiles' / 'corn_base.apsimx'
+    contents = json_file_path.read_text()
+    nameout = join(file_path, 'corn_base.apsimx')
+    with open(nameout, "w+") as openfile:
+        openfile.write(contents)
+    return nameout
+
+
+def _get_maize(file_path):
+    resource_directory = files('apsimNGpy')
+    json_file_path = resource_directory / 'basefiles' / 'maize.apsimx'
+    contents = json_file_path.read_text()
+    nameout = join(file_path, 'maize.apsimx')
+    with open(nameout, "w+") as openfile:
+        openfile.write(contents)
+    return nameout
+
+
+def _get_maize_no_till(file_path):
+    resource_directory = files('apsimNGpy')
+    json_file_path = resource_directory / 'basefiles' / 'maize_nt.apsimx'
+    contents = json_file_path.read_text()
+    nameout = join(file_path, 'maize_nt.apsimx')
+    with open(nameout, "w+") as openfile:
+        openfile.write(contents)
+    return nameout
+
+
+def _get_maize_NF_experiment(file_path):
+    resource_directory = files('apsimNGpy')
+    json_file_path = resource_directory / 'basefiles' / 'EXPERIMENT.apsimx'
+    contents = json_file_path.read_text()
+    nameout = join(file_path, 'EXPERIMENT.apsimx')
+    with open(nameout, "w+") as openfile:
+        openfile.write(contents)
+    return nameout
+
+
+def _get_maize_NF_experiment_NT(file_path):
+    resource_directory = files('apsimNGpy')
+    json_file_path = resource_directory / 'basefiles' / 'EXPERIMENT_NT.apsimx'
+    contents = json_file_path.read_text()
+    nameout = join(file_path, 'EXPERIMENT_NT.apsimx')
+    with open(nameout, "w+") as openfile:
+        openfile.write(contents)
+    return nameout
+
+
+def _get_SWIM(file_path):
+    resource_directory = files('apsimNGpy')
+    json_file_path = resource_directory / 'basefiles' / 'SWIM.apsimx'
+    contents = json_file_path.read_text()
+    nameout = join(file_path, 'SWIM.apsimx')
+    with open(nameout, "w+") as openfile:
+        openfile.write(contents)
+    return nameout
+
+
+class load_example_files():
+    def __init__(self, path):
+        """
+        path: string pathlike object where to copy the default example to
+        """
+        assert exists(path), "entered path does not exists please try again, \n ============================"
+        self.path = path
+
+    def _clean_up(self, path):
+        Path(f"{path}.db").unlink(missing_ok=True)
+        Path(f"{path}.db-shm").unlink(missing_ok=True)
+        Path(f"{path}.db-wal").unlink(missing_ok=True)
+        return path
+
+    @property
+    def get_maize_with_cover_crop(self):
+        self.weather_example = _weather(self.path)
+        return self._clean_up(_get_maize_example(self.path))
+
+    @property
+    def get_experiment_nitrogen_residue(self):
+        self.weather_example = _weather(self.path)
+        return self._clean_up(_get_maize_NF_experiment(self.path))
+
+    @property
+    def get_get_experiment_nitrogen_residue_NT(self):
+        self.weather_example = _weather(self.path)
+        return self._clean_up(_get_maize_NF_experiment_NT(self.path))
+
+    @property
+    def get_swim(self):
+        self.weather_example = _weather(self.path)
+        return self._clean_up(_get_SWIM(self.path))
+
+    @property
+    def get_maize(self):
+        self.weather_example = _weather(self.path)
+        return self._clean_up(_get_maize(self.path))
+
+    @property
+    def get_maize_no_till(self):
+        self.weather_example = _weather(self.path)
+        return self._clean_up(_get_maize_no_till(self.path))
+
+
+# APSIM defaults
+# TODO: incoporate other plafforms and also make itS detection faster
+# avoid hard codying
+@cache
+def detect_apsim_installation():
+    for rr, dd, ff in walk("C:/"):
+        for d in ff:
+            if d.startswith('Model') and d.endswith(".exe"):
+                f = join(rr, d)
+                if f is not None:
+                    return f
+                else:
+                    f = input("APSIM automatic APSIM detection failed, Please write the path to apsim installation: ")
+                    return f
+
+
+pat = detect_apsim_installation()
+if pat:
+    apsim = split(split(pat)[0])[0]
+    examples = join(apsim, 'Examples')
+dr = listdir(examples)
+print(dr)
+examples_files = {}
+for i in dr:
+    if i.endswith(".apsimx"):
+        name, ext = i.split(".")
+        examples_files[name] = join(examples, i)
+copy_path = join(getcwd(), 'apsim_default_model_examples')
+
+if not exists(copy_path):
+    mkdir(copy_path)
+
+weather_path = os.path.join(examples, "WeatherFiles")
+
+
+class DetectApsimExamples:
+    def __init__(self):
+        for name, file in examples_files.items():
+            setattr(self, name, name)
+
+    def get_example(self, crop):
+        cp = shutil.copy(examples_files[crop], join(copy_path, crop))
+        path = cp + '.apsimx'
+        apsim = SoilModel(path)
+        wp = os.path.join(weather_path, os.path.basename(apsim.show_met_file_in_simulation()))
+        apsim.replace_met_file(wp)
+        return apsim
+
+
+apsim_example = DetectApsimExamples()

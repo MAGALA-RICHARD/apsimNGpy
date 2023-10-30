5845e54e5a3796de914d3ad3907244bdefcf228f
MAGALA-RICHARD <rmagala640@gmail.com>
2023-10-30 15:08:14 -0500

    deleted

--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -3,5 +3,4 @@ import glob
 from apsimNGpy.weather import daymet_bylocation_nocsv, daymet_bylocation, daterange
 from apsimNGpy.base.base_data import load_example_files
 import apsimNGpy.manager.soilmanager as soil_manager
-
 __all__ = [daymet_bylocation_nocsv, daymet_bylocation, daterange, load_example_files, soil_manager]

55d54bd5ef451ad48d8d6d0b4432bb47c3f806eb
MAGALA-RICHARD <rmagala640@gmail.com>
2023-11-01 11:55:38 -0500

    download: soil tables from iterables

--- apsimNGpy/parallel/process.py
+++ apsimNGpy/parallel/process.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from os.path import dirname
 from os.path import join as opj
 from apsimNGpy.utililies.run_utils import run_model, read_simulation
-
+from apsimNGpy.manager.soilmanager import DownloadsurgoSoiltables, OrganizeAPSIMsoil_profile
 
 # _______________________________________________________________
 def run_apsimxfiles_in_parallel(iterable_files, ncores, use_threads=False):
@@ -76,3 +76,35 @@ def read_result_in_parallel(iterable_files, ncores, use_threads=False):
                 yield data
             progress.close()
         print(perf_counter() - a, 'seconds', f'to read {len(files)} apsimx database files')
+
+
+def download_soil_tables(iterable, use_threads=False):
+    def _concat(x):
+        try:
+            cod = iterable[x]
+            table = DownloadsurgoSoiltables(cod)
+            th = [150, 150, 200, 200, 200, 250, 300, 300, 400, 500]
+            sp = OrganizeAPSIMsoil_profile(table, thickness=20, thickness_values=th).cal_missingFromSurgo()
+            return {x: sp}
+        except Exception as e:
+            print("Exception Type:", type(e), "has occured")
+            print(repr(e))
+
+    if not use_threads:
+        with ThreadPoolExecutor(max_workers=10) as tpool:
+            futures = [tpool.submit(_concat, n) for n in range(len(iterable))]
+            progress = tqdm(total=len(futures), position=0, leave=True,
+                            bar_format='downloading soil_tables...: {percentage:3.0f}% completed')
+            for future in as_completed(futures):
+                progress.update(1)
+                yield future.result()
+            progress.close()
+    else:
+        with ProcessPoolExecutor(max_workers=3) as ppool:
+            futures = [ppool.submit(_concat, n) for n in range(len(iterable))]
+            progress = tqdm(total=len(futures), position=0, leave=True,
+                            bar_format='downloading soil_tables..: {percentage:3.0f}% completed')
+            for future in as_completed(futures):
+                progress.update(1)
+                yield future.result()
+            progress.close()

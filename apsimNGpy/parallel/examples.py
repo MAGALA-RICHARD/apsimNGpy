4891fb25b0aa08955d343e92195d98d0cbf0a1fe
MAGALA-RICHARD <rmagala640@gmail.com>
2023-11-02 14:51:43 -0500

    examples

--- apsimNGpy/parallel/examples.py
+++ apsimNGpy/parallel/examples.py
@@ -7,19 +7,19 @@ from apsimNGpy.utililies.utils import collect_runfiles
 from pathlib import Path
 hd = Path.home()
 os.chdir(hd)
-from apsimNGpy.base.base_data import DetectApsimExamples, apsim_example
-#We can all .apsim files in this directory
+
 files = collect_runfiles(path2files=hd, pattern=["*.apsimx"])
 if __name__ == "__main__":
-
+    # copy all examples to our workig directory
+    #ex = apsim_example.get_all_examples()
     run_apsimxfiles_in_parallel(files, ncores=10,use_threads=False)
     # files is an iterable or a generator
     # ncores is the numbe rof process or threads to use and use_threads will determine wthere we use threads or not
     # returns nothing
+    rpath = os.path.realpath(hd)
     # we can now read the results using the .db path as follow
-    files_db = collect_runfiles(path2files=hd, pattern=["*.db"])
+    files_db = collect_runfiles(path2files=rpath, pattern=["*.db"])
     dat = read_result_in_parallel(files_db, ncores=10)
     # return a generator object
     df = pd.concat(dat)
     print(df)
-    [apsim_example.get_example(i) for i in dir(apsim_example) if "__" not in i]

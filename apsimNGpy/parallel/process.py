2304df25cecb3ee7b0ffa29fd5fa619f7cf8e650
MAGALA-RICHARD <rmagala640@gmail.com>
2023-11-02 21:01:50 -0500

    long doc strings

--- apsimNGpy/parallel/process.py
+++ apsimNGpy/parallel/process.py
@@ -8,8 +8,9 @@ from os.path import join as opj
 from apsimNGpy.utililies.run_utils import run_model, read_simulation
 from apsimNGpy.manager.soilmanager import DownloadsurgoSoiltables, OrganizeAPSIMsoil_profile
 
+
 # _______________________________________________________________
-def run_apsimxfiles_in_parallel(iterable_files, ncores = None, use_threads=False):
+def run_apsimxfiles_in_parallel(iterable_files, ncores=None, use_threads=False):
     """
     files: lists of apsimx simulation files
     ncores  =no of cores or threads to use (integer)
@@ -20,7 +21,7 @@ def run_apsimxfiles_in_parallel(iterable_files, ncores = None, use_threads=False
     if ncores:
         ncore2use = ncores
     else:
-        ncore2use = int(cpu_count()*0.50)
+        ncore2use = int(cpu_count() * 0.50)
     if not use_threads:
         a = perf_counter()
         with ProcessPoolExecutor(ncore2use) as pool:
@@ -46,18 +47,49 @@ def run_apsimxfiles_in_parallel(iterable_files, ncores = None, use_threads=False
         print(perf_counter() - a, 'seconds', f'to run {len(files)} files')
 
 
-def read_result_in_parallel(iterable_files, ncores = None, use_threads=False):
+def read_result_in_parallel(iterable_files, ncores=None, use_threads=False):
     """
-    files: lists of apsimx simulation files
-    ncores  =no of cores or threads to use (integer)
-    use_thread: if true thread pool executor will be used if false processpool excutor will be called (boolean)
+
+    Read APSIMX simulation databases results from multiple files in parallel.
+
+    Args:
+    - iterable_files (list): A list of APSIMX db  files to be read in parallel.
+    - ncores (int, optional): The number of CPU cores or threads to use for parallel processing. If not provided, it defaults to 50% of available CPU cores.
+    - use_threads (bool, optional): If set to True, the function uses thread pool execution; otherwise, it uses process pool execution. Default is False.
+
+    Returns:
+    - generator: A generator yielding the simulation data read from each file.
+
+    Example:
+    ```python
+    # Example usage of read_result_in_parallel function
+    from your_module import read_result_in_parallel
+
+    simulation_files = ["file1.db", "file2.db", ...]  # Replace with actual database file names
+
+    # Using processes for parallel execution
+    result_generator = read_result_in_parallel(simulation_files, ncores=4, use_threads=False)
+
+    # Iterate through the generator to process results
+    for data in result_generator:
+        print(data)
+    it depends on the type of data but pd.concat could be a good option on the returned generator
+    ```
+
+    Notes:
+    - This function efficiently reads db file results in parallel.
+    - The choice of thread or process execution can be specified with the `use_threads` parameter.
+    - By default, the function uses 50% of available CPU cores or threads if `ncores` is not provided.
+    - Progress information is displayed during execution.
+    - Handle any exceptions that may occur during execution for robust processing.
     """
+
     # remove duplicates. because duplicates will be susceptible to race conditioning in paralell computing
     files = set(iterable_files)
     if ncores:
         ncore2use = ncores
     else:
-        ncore2use = int(cpu_count()*0.50)
+        ncore2use = int(cpu_count() * 0.50)
     if not use_threads:
         a = perf_counter()
         with ProcessPoolExecutor(ncore2use) as pool:
@@ -87,11 +119,44 @@ def read_result_in_parallel(iterable_files, ncores = None, use_threads=False):
         print(perf_counter() - a, 'seconds', f'to read {len(files)} apsimx database files')
 
 
-def download_soil_tables(iterable, use_threads=False, ncores =None, soil_series=None):
+def download_soil_tables(iterable, use_threads=False, ncores=None, soil_series=None):
     """
-    iterable: an iterable with lonlat coordnates as tuples or lists
-    return: calculated soil profiles with the corresponding index positions as a dictionary
+
+    Downloads soil data from SSURGO (Soil Survey Geographic Database) based on lonlat coordinates.
+
+    Args:
+    - iterable (iterable): An iterable containing lonlat coordinates as tuples or lists.
+    - use_threads (bool, optional): If True, use thread pool execution. If False, use process pool execution. Default is False.
+    - ncores (int, optional): The number of CPU cores or threads to use for parallel processing. If not provided, it defaults to 40% of available CPU cores.
+    - soil_series (None, optional): [Insert description if applicable.]
+
+    Returns:
+    - a generator: with dictionaries containing calculated soil profiles with the corresponding index positions based on lonlat coordinates.
+
+    Example:
+    ```python
+    # Example usage of download_soil_tables function
+    from your_module import download_soil_tables
+
+    lonlat_coords = [(x1, y1), (x2, y2), ...]  # Replace with actual lonlat coordinates
+
+    # Using threads for parallel processing
+    soil_profiles = download_soil_tables(lonlat_coords, use_threads=True, ncores=4)
+
+    # Iterate through the results
+    for index, profile in soil_profiles.items():
+        process_soil_profile(index, profile)
+    ```
+
+    Notes:
+    - This function efficiently downloads soil data and returns calculated profiles.
+    - The choice of thread or process execution can be specified with the `use_threads` parameter.
+    - By default, the function utilizes available CPU cores or threads (40% of total) if `ncores` is not provided.
+    - Progress information is displayed during execution.
+    - Handle any exceptions that may occur during execution to avoid aborting the whole download
+
     """
+
     def _concat(x):
         try:
             cod = iterable[x]
@@ -102,8 +167,9 @@ def download_soil_tables(iterable, use_threads=False, ncores =None, soil_series=
         except Exception as e:
             print("Exception Type:", type(e), "has occured")
             print(repr(e))
+
     if not ncores:
-        ncores_2use = int(cpu_count()*0.4)
+        ncores_2use = int(cpu_count() * 0.4)
         print(f"using: {ncores_2use} cpu cores")
     else:
         ncores_2use = ncores

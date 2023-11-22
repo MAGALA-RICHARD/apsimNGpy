60025410f1962c16a94c459823db0f464a13ee31
MAGALA-RICHARD <rmagala640@gmail.com>
2023-11-21 20:53:08 -0600

    dev version

--- apsimNGpy/utililies/tt.py
+++
@@ -1,9 +0,0 @@
-from apsimNGpy.utililies.utils import collect_runfiles
-from apsimNGpy.parallel.process import run_apsimxfiles_in_parallel
-from settings import DEFAULT_PATH
-
-path = collect_runfiles(DEFAULT_PATH, ["*Maize.apsimx", '*bean.apsimx'])
-
-
-if __name__ == "__main__":
-    run_apsimxfiles_in_parallel(path, ncores=5)

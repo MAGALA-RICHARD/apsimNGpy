7054fedf6209f9ac5a193d6f2dc8fe7b7784b8b8
MAGALA-RICHARD <rmagala640@gmail.com>
2023-12-07 22:36:33 -0600

    path access can now work across platforms

--- apsimNGpy/core/__init__.py
+++ apsimNGpy/core/__init__.py
@@ -1,5 +1,5 @@
-from apsimNGpy.core.pythonet_config import LoadPythonnet, get_apsim_path
+from apsimNGpy.core.pythonet_config import LoadPythonnet, APSIM_PATH
 from apsimNGpy.core.apsim import ApsimModel
 from apsimNGpy.core import weather
 from apsimNGpy.core.base_data import DetectApsimExamples, LoadExampleFiles
-__all__ = [LoadPythonnet, get_apsim_path, ApsimModel, weather, DetectApsimExamples, LoadExampleFiles]
+__all__ = [LoadPythonnet, APSIM_PATH, ApsimModel, weather, DetectApsimExamples, LoadExampleFiles]

3d4e6498a34b505383618529ea823c3c235dcc2f
MAGALA-RICHARD <rmagala640@gmail.com>
2023-12-08 01:23:06 -0600

    path access can now work across platforms

--- apsimNGpy/parallel/examples.py
+++ apsimNGpy/parallel/examples.py
@@ -10,6 +10,7 @@ hd = Path.home()
 os.chdir(hd)
 from apsimNGpy.core.base_data import LoadExampleFiles
 from apsimNGpy.utililies.utils import make_apsimx_clones
+
 # let's duplicate some files
 
 # let's copy two files here
@@ -17,10 +18,14 @@ data = LoadExampleFiles(hd)
 maize = data.get_maize
 nt_maize = data.get_maize_no_till
 ap = make_apsimx_clones(maize, 20)
+
+
 def fnn(x):
     return x
+
+
 # that is it all these files are now in the directory
-files = collect_runfiles(path2files=hd, pattern=["*.apsimx"])# change path as needed
+files = collect_runfiles(path2files=hd, pattern=["*.apsimx"])  # change path as needed
 
 if __name__ == "__main__":
     # copy all examples to our workig directory
@@ -32,8 +37,8 @@ if __name__ == "__main__":
     rpath = os.path.realpath(hd)
     # we can now read the results using the .db path as follow
     files_db = collect_runfiles(path2files=rpath, pattern=["*.db"])
-    dat = read_result_in_parallel(files_db, ncores=2, use_threads=True,report_name='MaizeR')
+    dat = read_result_in_parallel(files_db, ncores=2, use_threads=True, report_name='MaizeR')
     # return a generator object
     dl = list(dat)
-    #df = pd.concat(dat)
+    # df = pd.concat(dat)
     print(dl)

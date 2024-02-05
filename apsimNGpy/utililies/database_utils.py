7cedba99185ff03e0c601424f656e158b8550a9f
MAGALA-RICHARD <rmagala640@gmail.com>
2024-02-05 07:56:12 -0600

    new

--- apsimNGpy/utililies/database_utils.py
+++ apsimNGpy/utililies/database_utils.py
@@ -11,6 +11,7 @@ from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from functools import partial
 import pandas as pd
 from pandas import read_sql_query as rsq
+
 import sqlite3
 
 
@@ -49,7 +50,7 @@ def read_with_query(db, query):
     return df
 
 
-def read_db_table(db, table):
+def read_db_table(db, report_name):
     """
         Connects to a specified database, retrieves the entire contents of a specified table,
         and returns the results as a Pandas DataFrame.
@@ -81,7 +82,7 @@ def read_db_table(db, table):
         """
     # table = kwargs.get("table")
     conn = sqlite3.connect(db)
-    query = f"SELECT * FROM {table}"
+    query = f"SELECT * FROM {report_name}"
     df = rsq(query, conn)
     conn.close()
     return df
@@ -147,83 +148,7 @@ def _read_simulation(datastore, report_name=None):
         return dataframe_dict
 
 
-def convert_data(data):
-    data['Nitrogen'] = data['Nitrogen'].astype(int)
-    ky = list(data.columns)
-    if "Residue" in ky:
-        data['Residue'] = data['Residue'].astype(float)
-    return data
 
 
-def query_data(df, N, R=None):
-    if R:
-        dat = df.loc[(df['Nitrogen'] == N) & (df['Residue'] == R)]
-        # dat = df.query(f"Nitrogen== {N} and Residue == {R}")
-    else:
-        dat = df.loc[(df['Nitrogen'] == N)]
-    return dat
-
-
-# def query_data(df, N, R = None):
-#     #data  = df.to_records(index=False)
-#     if R:
-#         dat =df.query(f'Nitrogen == {N} and Residue =={R}')
-#         #dat = df.query(f"Nitrogen== {N} and Residue == {R}")
-#     else:
-#        dat = df.query(f'Nitrogen == {N}')
-#     return dat
-
-def excute_query_data(fileinput, N, R=None):
-    # we load apsimx file with .apsimx file extension because we want its corresponding data base
-    try:
-        meta = load_database(fileinput)
-        data = _read_simulation(meta.path)
-        carbon = convert_data(data['Carbon'])
-        # we query the data base by Residue and Nitrogen
-        maizer, annual = convert_data(data["MaizeR"]), convert_data(data["Annual"])
-        df_maize_yield, df_annual, df_carbon = query_data(maizer, N, R), query_data(annual, N, R), query_data(carbon, N,
-                                                                                                              R)
-        mean_n20 = np.mean(df_annual['TopN2O'])
-        MineralN = np.mean(df_annual['MineralN'])
-        leached_N = np.mean(df_annual['CumulativeAnnualLeaching'])
-        carbon = np.mean(df_carbon['changeincarbon']) * -1
-        Myield = np.mean(df_maize_yield['Yield'])
-        prof_maize = Myield / 25.40 * 4.6650
-        revenue = prof_maize
-        Ncost = 1.8291246 * N
-        profit = revenue - Ncost
-        ghg_co2_co2eq = -1 * carbon * 44 / 128 * 1
-        ghg_n02_co2eq = mean_n20 * 44 / 28 * 298
-        co2_eq = ghg_co2_co2eq + ghg_n02_co2eq
-        data = meta.path, mean_n20, carbon * -1, MineralN, leached_N, profit * -1, co2_eq, Myield * -1
-        return data
-    except Exception as e:
-        print(repr(e))
-
-
-def excute_querry_in_pool(data):
-    """
-    :param data: tuple of path and N amountd
-    :return: tuple
-    """
-    meta_data = namedtuple('db', ['path', 'N'])
-    generate_data = meta_data(path=data[0], N=data[1])
-    dt = excute_query_data(fileinput=generate_data.path, N=generate_data.N)
-    return dt
-
-
-def read_data_in_parallel(aps_list, Nitrogen, Residue=None):
-    """
-    :param aps_list: list of apsimx data bases
-    :param Nitrogen: integer
-    :param Residue:  float
-    :return: pandas data frame
-    """
-    read_partial = partial(excute_query_data, N=Nitrogen, R=Residue)
-    with ProcessPoolExecutor(max_workers=8) as tpool:
-        tp = tpool.map(read_partial, aps_list)
-        tp = [i for i in tp if i is not None]
-        data = pd.DataFrame(list(tp))
-    return data.mean(0, numeric_only=True)
 
 

8765f9742f47cebfa4653ec2c4454a92f5027434
MAGALA-RICHARD <rmagala640@gmail.com>
2024-02-05 12:17:27 -0600

    new

--- apsimNGpy/core/simulation.py
+++ apsimNGpy/core/simulation.py
@@ -1,8 +1,10 @@
+import numpy as np
 from typing import Tuple, Any
 from apsimNGpy.core.apsim import ApsimModel
 from apsimNGpy.core.base_data import LoadExampleFiles
 from pathlib import Path
 from apsimNGpy.weather import daymet_bylocation_nocsv, daymet_bylocation
+from apsimNGpy.manager.soilmanager import DownloadsurgoSoiltables, OrganizeAPSIMsoil_profile
 
 wd = Path.home()
 from os.path import basename
@@ -21,53 +23,40 @@ def simulate(model: Any, location: Tuple[float, float], read_from_string=True, s
      soil_series: str
      kwargs:
         copy: bool = False, out_path: str = None, read_from_string=True,
+
         soil_series: str = 'domtcp', thickness: int = 20, bottomdepth: int = 200,
+
         thickness_values: list = None, run_all_soils: bool = False
+
         report_name: str specifies the report or table name in the simulation, for which to read the reasults
-        replace_weather: bool if true,weather is downloaded using the specified location and itis replaced
+
+        replace_weather: Set this boolean to true to download and replace the weather data based on the specified location.
+
+        replace_soil: Set this boolean to true to download and replace the soil data using the given location details.
+
+        managegement practices: Provide a list of management decissions
 
     """
-
+    thi = [150, 150, 200, 200, 200, 250, 300, 300, 400, 500]
+    th = kwargs.get("thickness_values", thi)
     simulator_model = ApsimModel(
-        model, copy=kwargs.get('copy'), read_from_string=read_from_string, lonlat=location)
+        model, copy=kwargs.get('copy'), read_from_string=read_from_string, lonlat=location, thickness_values=th)
 
     report_name = kwargs.get("report_name")
     sim_name = simulator_model.extract_simulation_name
     if kwargs.get('replace_weather', False):
-        wname = 'w' + model.strip('apsimx') + ".met"
-        wf = daymet_bylocation_nocsv(location, start, end, filename='wname.met')
-        ApsimModel.replace_met_file(wf, sim_name)
-    met = simulator_model.show_met_file_in_simulation()
-    print(met)
+        wname = model.strip('.apsimx') + '_w.met'
+        wf = daymet_bylocation_nocsv(location, start, end, filename=wname)
+        simulator_model.replace_met_file(wf, sim_name)
+    if kwargs.get("replace_soil", False):
+        table = DownloadsurgoSoiltables(location)
+        sp = OrganizeAPSIMsoil_profile(table, thickness=20, thickness_values=th)
+        sp = sp.cal_missingFromSurgo()
+        simulator_model.replace_downloaded_soils(sp, sim_name)
     simulator_model.run()
-    return simulator_model.results
+    return simulator_model
 
 
-import os
-# pp = simulate(maize, lon, replace_weather = True)
-import os
-
-
-def search(directory):
-    am = []
-    for root, dirs, files in os.walk(directory):
-
-        for file in files:
-            if file.endswith('.py'):
-                file_path = os.path.join(root, file)
-                with open(file_path, 'r', encoding='latin-1') as f:
-                    if 'nasa' in f.read():
-                        print(file_path)
-                        am.append(file_path)
-    return am  # Return the path of the first .py file that contains 'nasapower'
-    return None  # Return None if 'nasapower' is not found in any .py files
-
-
-# Usage
-directory_to_search = r"C:\Users\rmagala\Box\apsimNGpy"
-result = search(directory_to_search)
-if result:
-    print(f"Found 'nasapower' in: {result}")
-else:
-    print("Did not find 'nasapower' in any .py files.")
-
+md = {"Name": 'PostharvestillageMaize', 'Fraction': 0.001
+      }
+pp = simulate(maize, lon, replace_weather=True, replace_soil=True)

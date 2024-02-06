3b7d1fc970d3a0cadbdea732623359ff32956f02
MAGALA-RICHARD <rmagala640@gmail.com>
2024-02-05 22:06:08 -0600

    simulate from a shape file

--- apsimNGpy/core/simulation.py
+++ apsimNGpy/core/simulation.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from apsimNGpy.utililies.utils import select_process
 from apsimNGpy.weather import daymet_bylocation_nocsv, daymet_bylocation
 from apsimNGpy.manager.soilmanager import DownloadsurgoSoiltables, OrganizeAPSIMsoil_profile
-from apsimNGpy.utililies.spatial import create_fishnet1, create_apsimx_sim_files
+from apsimNGpy.utililies.spatial import create_fishnet1, create_apsimx_sim_files, generate_random_points
 from tqdm import tqdm
 
 wd = Path.home()
@@ -48,19 +48,19 @@ def simulate_single_point(model: Any, location: Tuple[float, float], report, rea
     th = kwargs.get("thickness_values", thi)  # in case it is not supplied, we take thi
     simulator_model = ApsimModel(
         model, copy=kwargs.get('copy'), read_from_string=read_from_string, lonlat=location, thickness_values=th)
-
+    # if replace weather
     sim_name = simulator_model.extract_simulation_name
     if kwargs.get('replace_weather', False):
         wname = model.strip('.apsimx') + '_w.met'
         wf = daymet_bylocation_nocsv(location, start, end, filename=wname)
         simulator_model.replace_met_file(wf, sim_name)
-
+    # replace soil is true
     if kwargs.get("replace_soil", False):
         table = DownloadsurgoSoiltables(location)
         sp = OrganizeAPSIMsoil_profile(table, thickness=20, thickness_values=th)
         sp = sp.cal_missingFromSurgo()
         simulator_model.replace_downloaded_soils(sp, sim_name)
-
+    # if replace management practices
     if kwargs.get("mgt_practices"):
         simulator_model.update_mgt(kwargs.get('mgt_practices'), sim_name)
     simulator_model.run(report_name=report)
@@ -94,14 +94,19 @@ def simulate_single_from_shape_file(wd, shape_file, model: Any, resolution, repo
         
         ncores: set the number of cores
         use_thread: set true to run in parallel processing
+        random_grid_points: bolean. set this true to sample specified poitns per grid of your choice
+        num_points: int,  set number of points
 
     """
-    arr = create_fishnet1(shape_file, lon_step=resolution, lat_step=resolution, ncores=kwargs.get('ncores', 4))
+    use_thread, ncores, num_points = kwargs.get('use_thread', True), kwargs.get('ncores', 3), kwargs.get('num_points',
+                                                                                                         2)
+    if kwargs.get('random_grid_points'):
+        arr = generate_random_points(shape_file, resolution, ncores, num_points)
+    else:
+        arr = create_fishnet1(shape_file, lon_step=resolution, lat_step=resolution, ncores=kwargs.get('ncores', 4))
     df = create_apsimx_sim_files(wd, model, arr)
     thi = [150, 150, 200, 200, 200, 250, 300, 300, 400, 500]
     th = kwargs.get("thickness_values", thi)  # in case it is not supplied, we take thi
-
-    use_thread, ncores = kwargs.get('use_thread', True), kwargs.get('ncores', 3)
 
     def worker(row):
         ID = row['ID']
@@ -141,8 +146,8 @@ def simulate_single_from_shape_file(wd, shape_file, model: Any, resolution, repo
 md = {"Name": 'PostharvestillageMaize', 'Fraction': 0.001
       }
 if __name__ == '__main__':
-        pp = simulate_single_point(maize, lon, replace_weather=True, replace_soil=True, mgt_practices=md, report='MaizeR')
-        shp = r'D:\ACPd\Bear creek simulations\bearcreek_shape\bearcreek.shp'
-        wd = r'C:\Users\rmagala'
-        lp = simulate_single_from_shape_file(wd, shp, maize, 1000, report = 'MaizeR', replace_soil=True, replace_weather=True)
-        lip = list(lp)
+    pp = simulate_single_point(maize, lon, replace_weather=True, replace_soil=True, mgt_practices=md, report='MaizeR')
+    shp = r'D:\ACPd\Bear creek simulations\bearcreek_shape\bearcreek.shp'
+    wd = r'C:\Users\rmagala'
+    lp = simulate_single_from_shape_file(wd, shp, maize, 1200, report='MaizeR', random_grid_points=True, replace_soil=True, replace_weather=True)
+    lip = list(lp)

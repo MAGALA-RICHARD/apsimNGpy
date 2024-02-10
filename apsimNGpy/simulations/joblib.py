ebb7e2e26c279ba8981d6d66b1f8a82c14c551bb
MAGALA-RICHARD <rmagala640@gmail.com>
2024-02-09 20:50:50 -0600

    ssample by polygons

--- apsimNGpy/simulations/joblib.py
+++ apsimNGpy/simulations/joblib.py
@@ -206,10 +206,82 @@ def sample_by_polygons(shp, k=1, filter_by=None, filter_value=None):
         points.extend(pts)
 
     points_gdf = gpd.GeoDataFrame(geometry=points, crs=CRS)
-    gdf = points_gdf.to_crs("EPSG:4326")  # Convert to WGS84 CRS
-
+    new_gdf = points_gdf.sjoin(gd)
+    ndf = new_gdf.to_crs("EPSG:4326", inplace=False)
+    # convert geometry back to original projection
     print(f"Total points generated: {np.sum(poi)}")
-    return np.array([(point.x, point.y) for point in gdf.geometry])
+    ndf['lonlat'] = [(point.x, point.y) for point in ndf.geometry]
+    ndf.to_crs(CRS, inplace=True)
+    return ndf
+
+
+def match_crop(abb, add_wheat=None):
+    """
+    Converts agricultural abbreviations to a string of crop names, with an option to insert 'Wheat' at specified
+    intervals. The function interprets abbreviations where 'B' stands for Soybean, 'C' for Maize, 'P' for Pasture,
+    'U' for Developed areas, 'T' for Wetlands, 'F' for Forest, and 'R' for Other crops.
+
+    This function takes an abbreviation string representing a sequence of crops and optionally inserts 'Wheat' into
+    the sequence at predetermined positions. It handles special cases where the abbreviation matches certain
+    patterns, returning None for these cases.
+
+    Parameters:
+    - abb (str): The abbreviation string representing a sequence of crops. Each character stands for a specific crop:
+        'C' for Maize,
+        'B' for Soybean,
+        'W' for Wheat,
+        'P' for Tropical Pasture.
+      Characters 'U', 'R', 'T', and 'F' are ignored and removed from the abbreviation.
+    - add_wheat (bool, optional): If True, 'Wheat' is inserted at positions 1, 3, 5, 7, 9, and 11 in the sequence. Defaults to None, which means 'Wheat' is not added.
+
+    Returns:
+    - str or None: A comma-separated string of crop names based on the abbreviation. Returns None if the abbreviation matches special cases 'TTTTTT' or 'UUUUUUU'.
+
+    Example:
+    ```
+    # Convert abbreviation to crop names with additional 'Wheat'
+    crop_sequence = match_crop("CBWP", add_wheat=True)
+    print(crop_sequence)
+    # Output: "Maize,Wheat,Soybean,Wheat,Wheat,Tropical Pasture"
+
+    # Convert abbreviation without adding 'Wheat'
+    crop_sequence = match_crop("CBWP")
+    print(crop_sequence)
+    # Output: "Maize,Soybean,Wheat,Tropical Pasture"
+    ```
+
+    Note:
+    - The function returns None for special case abbreviations 'TTTTTT' and 'UUUUUUU' as an indication of non-standard sequences that should not be processed.
+    - The insertion of 'Wheat' at specified positions assumes the sequence is long enough to accommodate these insertions. If the sequence is shorter, 'Wheat' will only be inserted up to the length of the sequence.
+    """
+    # Reserve the original abbreviation
+    reserve = abb
+
+    # Check if abbreviation does not match special cases and contains certain characters
+    if abb not in ['TTTTTT', 'UUUUUUU'] and ('C' in abb or 'B' in abb or 'W' in abb or 'P' in abb):
+        # Remove specified characters from the abbreviation
+        work = abb.replace("U", "").replace("R", "").replace("T", "").replace("F", "")
+
+        # Replace abbreviations with crop names, adding commas between them
+        rotation = work.replace("C", "Maize,").replace("B", "Soybean,").replace("W", "Wheat").replace('P',
+                                                                                                      'TropicalPasture').rstrip(
+            ",")
+        rot2 = rotation.split(",")
+
+        if add_wheat:
+            # Insert "Wheat" at specified positions if add_wheat is True
+            for i in [1, 3, 5, 7, 9, 11]:
+                if i < len(rot2):
+                    rot2.insert(i, "Wheat")
+            crop = ",".join(rot2)
+        else:
+            # Join the rotation without additional "Wheat"
+            crop = ",".join(rot2)
+    else:
+        # If abbreviation matches special cases, return it as is
+        crop = None
+
+    return crop
 
 
 def create_apsimx_sim_files(wd, model, iterable):

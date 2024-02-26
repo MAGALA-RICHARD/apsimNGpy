34d0e269919db35085295d0a1c3b136627d294fb
MAGALA-RICHARD <rmagala640@gmail.com>
2024-02-26 16:31:40 -0600

    download soils from web

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 import sys
 import os
+
 VERSION = '0.0.3'
 DESCRIPTION = 'apsimx next generation package interface'
 LONG_DESCRIPTION = 'run, edit, download soils and weather and interact with the apsimx file'
@@ -8,7 +9,7 @@ with open('README.rst') as readme_file:
     readme = readme_file.read()
 setup(
     name='apsimNGpy',
-    version='0.0.21',
+    version='0.0.25',
     url='https://github.com/MAGALA-RICHARD/apsimNGpy.git',
     license='MIT',
     author='Richard Magala',
@@ -17,27 +18,29 @@ setup(
     long_description=readme,
     packages=find_packages(),
     include_package_data=True,
-    package_data={'': ['./apsimNGpy/data/*.apsimx', './apsimNGpy/*.met','./apsimNGpy/examples/*.png']},
-     keywords=['python', 'APSIM Next Generation', 'pythonnet', 'crop modeling'],
-     classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Education",
-            "Programming Language :: Python :: 3",
-            "Operating System :: Microsoft :: Windows",
-        ],
-     install_requires=[
+    package_data={'': ['./apsimNGpy/data/*.apsimx', './apsimNGpy/*.met', './apsimNGpy/examples/*.png']},
+    keywords=['python', 'APSIM Next Generation', 'pythonnet', 'crop modeling'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    install_requires=[
         'numpy>=1.20.0',
         'scipy>=1.7',
-         'xmltodict >=0.13.0',
-         'requests >=2.31.0',
-         'pythonnet >= 3.0.1',
-         'rasterio >=1.3.8',
-         'shapely >=2.0.1',
-         'clr >= 1.0.3',
-         'geopandas >=0.13.2',
-         'pandas >=2.0.3'
-
+        'xmltodict >=0.13.0',
+        'requests >=2.31.0',
+        'pythonnet >= 3.0.1',
+        'rasterio >=1.3.8',
+        'shapely >=2.0.1',
+        'clr >= 1.0.3',
+        'geopandas >=0.13.2',
+        'pandas >=2.0.3',
+        'geopy >= 2.4.1',
+        'tqdm >= 4.66.2',
+        'progressbar >= 2.5',
+        'joblib >= 1.3.2'
 
     ]
 )
-

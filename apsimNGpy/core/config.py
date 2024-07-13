96b06781b04437622252a7d153eb46bfd2a3669d
MAGALA-RICHARD <rmagala640@gmail.com>
2024-07-12 23:53:40 -0500

    config apsim. users can set the apsim bin folder path

---
+++ apsimNGpy/core/config.py
@@ -0,0 +1,20 @@
+from os.path import dirname, realpath
+
+from pymoo.version import __version__
+
+
+class Config:
+    """
+    The configuration of this aPsimNGpy in general providing the place
+    for declaring global variables.
+    """
+
+    ApSIM_LOCATION = None
+
+    @classmethod
+    def set_aPSim_bin_path(cls, path):
+        cls.software_path = path
+
+    @classmethod
+    def get_aPSim_bin_path(cls):
+        return cls.ApSIM_LOCATION

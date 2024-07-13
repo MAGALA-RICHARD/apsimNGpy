717b11a7373e831c30c8164731b17582d2def1f0
MAGALA-RICHARD <rmagala640@gmail.com>
2024-07-13 12:54:51 -0500

    config apsim. users can set the apsim bin folder path

--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -1,7 +1,7 @@
 from apsimNGpy import core
 from apsimNGpy.utililies import utils
 from apsimNGpy.core.apsim import ApsimModel
-from apsimNGpy.core.base_data import LoadExampleFiles, LoadPythonnet
+
 from apsimNGpy.parallel.process import custom_parallel
 from apsimNGpy.validation.evaluator import validate
 from apsimNGpy.manager import soilmanager, soil_queries
@@ -9,8 +9,8 @@ from apsimNGpy.experiment.variable import BoundedVariable, Variable, DiscreteVar
 from apsimNGpy.manager.weathermanager import daymet_bylocation_nocsv, validate_met
 from apsimNGpy.manager.soilmanager import DownloadsurgoSoiltables, OrganizeAPSIMsoil_profile
 
-__all__ = [core, utils, custom_parallel, LoadExampleFiles,
+__all__ = [core, utils, custom_parallel,
            DiscreteVariable, BoundedVariable, Variable,
            daymet_bylocation_nocsv, validate_met,
            OrganizeAPSIMsoil_profile, DownloadsurgoSoiltables,
-           ApsimModel, LoadPythonnet, soilmanager, soil_queries, soil_queries]
+           ApsimModel, soilmanager, soil_queries, soil_queries]

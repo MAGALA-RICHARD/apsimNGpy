12977a0ad256aedb122de6ba426c1381fbbf211e
MAGALA-RICHARD <rmagala640@gmail.com>
2024-07-14 11:28:33 -0500

    added uncertainity estimate or automatic parameter calibration

--- apsimNGpy/core/__init__.py
+++ apsimNGpy/core/__init__.py
@@ -1,6 +1,5 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from apsimNGpy.core.pythonet_config import LoadPythonnet, aPSim_PATH
 from apsimNGpy.core.apsim import ApsimModel
 from apsimNGpy.core import weather
 from apsimNGpy.core.base_data import DetectApsimExamples, LoadExampleFiles
-__all__ = [LoadPythonnet, aPSim_PATH, ApsimModel, weather, DetectApsimExamples, LoadExampleFiles]
+__all__ = [ApsimModel, weather, DetectApsimExamples, LoadExampleFiles]

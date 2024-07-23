794ea6b2fe668cd2a17c266b3a0cc96dd38b4aec
MAGALA-RICHARD <rmagala640@gmail.com>
2024-07-22 20:26:48 -0500

    config apsim. users can set the apsim bin folder path

--- apsimNGpy/settings.py
+++ apsimNGpy/settings.py
@@ -1,6 +1,12 @@
 from multiprocessing import cpu_count
+from enum import Enum
 
-WGS84 = 'epsg:4326'
-CORES = int(cpu_count() * 0.6)
-SOIL_THICKNESS = [150, 150, 200, 200, 200, 250, 300, 300, 400, 500]
-CRS = 'EPSG:26915'
+class ConstantSettings(Enum):
+    
+    WGS84 = 'epsg:4326'
+    
+    CORES = int(cpu_count() * 0.6)
+    
+    SOIL_THICKNESS = [150, 150, 200, 200, 200, 250, 300, 300, 400, 500]
+    
+    CRS = 'EPSG:26915'

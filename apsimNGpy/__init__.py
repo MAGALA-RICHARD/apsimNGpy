f5dd45e8f30fcb9dfc842b185664f55740fb39a5
MAGALA-RICHARD <rmagala640@gmail.com>
2024-07-22 20:55:01 -0500

    Models.Core.ApsimFile.ConverterReturnType

--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -1,16 +1,16 @@
-from apsimNGpy import core
+
 from apsimNGpy.utililies import utils
-from apsimNGpy.core.apsim import ApsimModel
+
 
 from apsimNGpy.parallel.process import custom_parallel
-from apsimNGpy.validation.evaluator import validate
+
 from apsimNGpy.manager import soilmanager, soil_queries
 from apsimNGpy.experiment.variable import BoundedVariable, Variable, DiscreteVariable
 from apsimNGpy.manager.weathermanager import daymet_bylocation_nocsv, validate_met
 from apsimNGpy.manager.soilmanager import DownloadsurgoSoiltables, OrganizeAPSIMsoil_profile
 
-__all__ = [core, utils, custom_parallel,
+__all__ = [ utils, custom_parallel,
            DiscreteVariable, BoundedVariable, Variable,
            daymet_bylocation_nocsv, validate_met,
            OrganizeAPSIMsoil_profile, DownloadsurgoSoiltables,
-           ApsimModel, soilmanager, soil_queries, soil_queries]
+            soilmanager, soil_queries, soil_queries]

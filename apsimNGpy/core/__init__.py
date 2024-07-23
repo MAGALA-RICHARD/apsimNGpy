cca432f48dc1e77cac59633194bce78eaf89cc23
MAGALA-RICHARD <rmagala640@gmail.com>
2024-07-22 20:55:01 -0500

    Models.Core.ApsimFile.ConverterReturnType

--- apsimNGpy/core/__init__.py
+++
@@ -1,5 +0,0 @@
-import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-from apsimNGpy.core.apsim import ApsimModel
-from apsimNGpy.core import weather
-
-__all__ = [ApsimModel, weather]

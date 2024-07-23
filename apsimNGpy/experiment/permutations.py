2adf91b17dee8bf0d46a71ba6be55d826f8ae8ee
MAGALA-RICHARD <rmagala640@gmail.com>
2024-07-22 20:26:48 -0500

    config apsim. users can set the apsim bin folder path

--- apsimNGpy/experiment/permutations.py
+++ apsimNGpy/experiment/permutations.py
@@ -1,11 +1,13 @@
 from itertools import product
 from collections import defaultdict
+
 grouped = defaultdict(dict)
 
 factors = ['crop_code', 'Nitrogen', "ResidueFractions", "ApplicationType", "Tillage", "Clusters"]
 
-def create_permutations(factors:list, factor_names: list):
-        """_summary_
+
+def create_permutations(factors: list, factor_names: list):
+    """_summary_
 
         The create_permutations function is designed to generate a dictionary of permutations from
         a list of factors, with each permutation indexed by an enumeration and the factor values labeled
@@ -29,68 +31,73 @@ def create_permutations(factors:list, factor_names: list):
         ValueError: If the length of factor_names does not match the length of factors, indicating a mismatch between the number of
          provided factor names and the number of factors. The error message is: "Unacceptable - factor names should have the same length as the factor list."
         """
-    
-        if len(factor_names) != len(factor_names):
-            raise ValueError("Un acceptable factor names should have the same length as the factor list ")
-        permutations = list(product(*factors))
-        
-        # attach simulation_id
-        return  {i:dict(zip(factor_names, m)) for i, m in enumerate(permutations)}
+
+    if len(factor_names) != len(factor_names):
+        raise ValueError("Un acceptable factor names should have the same length as the factor list ")
+    permutations = list(product(*factors))
+
+    # attach simulation_id
+    return {i: dict(zip(factor_names, m)) for i, m in enumerate(permutations)}
+
+
 class GenerateCombinations:
     def __init__(self, management_factors):
-          self.mgt = management_factors
+        self.mgt = management_factors
+
     @property
     def management_combination(self):
-        return  create_permutations([n.variables for n in self.mgt], [i.parameter for i in self.mgt])
+        return create_permutations([n.variables for n in self.mgt], [i.parameter for i in self.mgt])
+
     @property
     def organise_scrips(self):
-        scp = [i.get_script_manager for i in self.mgt if i.get_script_manager['Name']!='NA' ]
+        scp = [i.get_script_manager for i in self.mgt if i.get_script_manager['Name'] != 'NA']
 
         return scp
 
+
 def mgt_updater(simId, perms, old_list):
-   """
+    """
     old_list (list) is a list of ALL MANAGEMENT scripts dictionary with key value pairs
     perms (dict) is the dictionary returned by the permutation methods,
     simId (int) is the target id of the simulation
 
     """
-   upd = perms[simId]
-   upd = list(perms[simId].keys())
-   for new, old in zip(upd, old_list):
+    upd = perms[simId]
+    upd = list(perms[simId].keys())
+    for new, old in zip(upd, old_list):
 
-       if new in old.keys():
-          old[new] = perms[simId][new]
-   di = old_list.copy()
-   for item in di:
-       # Merge dictionaries with the same 'Name' into grouped
-       if 'Name' in item:
-           name = item['Name']
-           grouped[name].update(item)
+        if new in old.keys():
+            old[new] = perms[simId][new]
+    di = old_list.copy()
+    for item in di:
+        # Merge dictionaries with the same 'Name' into grouped
+        if 'Name' in item:
+            name = item['Name']
+            grouped[name].update(item)
 
-   # Convert the grouped dictionary back to a list of dictionaries
-   mg_list = list(grouped.values())
-   return mg_list
+    # Convert the grouped dictionary back to a list of dictionaries
+    mg_list = list(grouped.values())
+    return mg_list
 
 
 if __name__ == "__main__":
-       def generateFactors():
-          mn =  {'Name': "Simple Rotation", 'Parameters': ["Crops", "Cropb"], 'Variables':['Maize, Soybean', 'Maize, Wheat']}
-          org ={i: m for i in mn.get('Parameters') for m in mn.get('Variables')}
-          org['Name']= mn.get('Name')
-          return org
-       print(generateFactors())
-       from variable import  DiscreteVariable, BoundedVariable
-       N = DiscreteVariable(options=[100, 200], place_holder_name='Nitrogen', parameter='Amount', manager='NitrogenManger')
-       Dep = BoundedVariable(bounds = (0, 350), place_holder_name='depth', manager='Tillage', parameter='Depth')
-       comb = GenerateCombinations([N, Dep])
-       ap= comb.management_combination
-       from apsimNGpy.manager.soilmanager import DownloadsurgoSoiltables
-       lon  = -119.72330, 36.92204
-       xp = DownloadsurgoSoiltables(lon)
-       print(xp)
+    def generateFactors():
+        mn = {'Name': "Simple Rotation", 'Parameters': ["Crops", "Cropb"],
+              'Variables': ['Maize, Soybean', 'Maize, Wheat']}
+        org = {i: m for i in mn.get('Parameters') for m in mn.get('Variables')}
+        org['Name'] = mn.get('Name')
+        return org
 
 
+    print(generateFactors())
+    from variable import DiscreteVariable, BoundedVariable
 
+    N = DiscreteVariable(options=[100, 200], place_holder_name='Nitrogen', parameter='Amount', manager='NitrogenManger')
+    Dep = BoundedVariable(bounds=(0, 350), place_holder_name='depth', manager='Tillage', parameter='Depth')
+    comb = GenerateCombinations([N, Dep])
+    ap = comb.management_combination
+    from apsimNGpy.manager.soilmanager import DownloadsurgoSoiltables
 
-
+    lon = -119.72330, 36.92204
+    xp = DownloadsurgoSoiltables(lon)
+    print(xp)

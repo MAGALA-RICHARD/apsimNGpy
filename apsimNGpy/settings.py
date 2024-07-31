43c22e9ef0ad15b77185e9af4b873014cde47100
MAGALA-RICHARD <rmagala640@gmail.com>
2024-07-30 21:32:46 -0500

    import

--- apsimNGpy/settings.py
+++ apsimNGpy/settings.py
@@ -1,12 +1,13 @@
 from multiprocessing import cpu_count
-from enum import Enum
+from dataclasses import dataclass
 
-class ConstantSettings(Enum):
-    
+
+@dataclass
+class ConstantSettings:
     WGS84 = 'epsg:4326'
-    
-    CORES = int(cpu_count() * 0.6)
-    
+
+    CORES: int = int(cpu_count() * 0.6)
+
     SOIL_THICKNESS = [150, 150, 200, 200, 200, 250, 300, 300, 400, 500]
-    
+
     CRS = 'EPSG:26915'

89963d3147889d62145fadd9281397e067b3e836
MAGALA-RICHARD <rmagala640@gmail.com>
2024-08-26 15:37:13 -0500

    repalce_soilwater

---
+++ apsimNGpy/core/soil_helper.py
@@ -0,0 +1,21 @@
+from apsimNGpy.core.pythonet_config import LoadPythonnet
+from Models.Soils import Soil, Physical, SoilCrop, Organic, Solute, Chemical
+
+
+def soil_components(component):
+    _comp = component.lower()
+    comps = {'organic': Organic,
+             'physical': Physical,
+             'soilcrop': SoilCrop,
+             'solute': Solute,
+             'chemical': Chemical,
+             }
+    return comps[_comp]
+
+
+# physical child parameters
+physical_parameters = {'BD', "DUL", "Sand", 'Clay', 'Silt', "SAT", 'AirDry', 'LL15', 'SW', 'KS', 'Rocks', "Depth"}
+physicals = {key: Physical for key in physical_parameters}
+# organic child parameters
+organic_parameters = {'Carbon', "FBiom", "FInert", 'FOM', "Depth", 'SoilCNRatio'}
+organics = {key: Organic for key in organic_parameters}

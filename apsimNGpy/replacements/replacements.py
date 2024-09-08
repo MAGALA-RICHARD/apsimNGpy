980246c2b3108c7b28cc02dc576bb064d8977d34
MAGALA-RICHARD <rmagala640@gmail.com>
2024-09-08 00:07:48 -0500

    repalce-soil_values_by_path

--- apsimNGpy/replacements/replacements.py
+++ apsimNGpy/replacements/replacements.py
@@ -15,7 +15,7 @@ class ReplacementHolder(APSIMNG, ABC):
         self.out_path = out_path
 
     @abstractmethod
-    def update_child_params(self, child: str, **kwargs):
+    def update_mgt_by_path(self, **kwargs):
         """Abstract method to replace parameters for a single child node"""
         pass
 
@@ -67,7 +67,111 @@ class Replacements(ReplacementHolder):
         else:
             raise TypeError(f"Unknown node: {child}, children should be any of {self._methods.keys()}")
 
-    def update_mgt_by_path(self, *, path, param_values, fmt='.'):
+    def replace_soil_properties_by_path(self, path: str,
+                                        param_values: list,
+                                        str_fmt=".",
+                                        **kwargs):
+        # TODO I know there is a better way to implement this
+        """
+        This function processes a path where each component represents different nodes in a hierarchy,
+        with the ability to replace parameter values at various levels.
+
+        :param path:
+            A string representing the hierarchical path of nodes in the order:
+            'simulations.Soil.soil_child.crop.indices.parameter'. Soil here is a constant
+
+            - The components 'simulations', 'crop', and 'indices' can be `None`.
+            - Example of a `None`-inclusive path: 'None.Soil.physical.None.None.BD'
+            - If `indices` is a list, it is expected to be wrapped in square brackets.
+            - Example when `indices` are not `None`: 'None.Soil.physical.None.[1].BD'
+            - if simulations please use square blocks
+               Example when `indices` are not `None`: '[maize_simulation].physical.None.[1].BD'
+
+            **Note: **
+            - The `soil_child` node might be replaced in a non-systematic manner, which is why indices
+              are used to handle this complexity.
+            - When a component is `None`, default values are used for that part of the path. See the
+              documentation for the `replace_soil_property_values` function for more information on
+              default values.
+
+        :param param_values:
+            A list of parameter values that will replace the existing values in the specified path.
+            For example, `[0.1, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08]` could be used to replace values for `NH3`.
+
+        :param str_fmt:
+            A string specifying the formatting character used to separate each component in the path.
+            Examples include ".", "_", or "/". This defines how the components are joined together to
+            form the full path.
+
+        :return:
+            Returns the instance of `self` after processing the path and applying the parameter value replacements.
+
+            Example f
+
+            from apsimNGpy.core.base_data import load_default_simulations
+            model = load_default_simulations(crop = 'maize')
+            model.replace_soil_properties_by_path(path = 'None.Soil.Organic.None.None.Carbon', param_values= [1.23])
+            if we want to replace carbon at the bottom of the soil profile, we use a negative index  -1
+            model.replace_soil_properties_by_path(path = 'None.Soil.Organic.None.[-1].Carbon', param_values= [1.23])
+        """
+
+        function_parameters = ['simulations', 'Soil', 'soil_child', 'crop', 'indices', 'parameter']
+        expected_nones = ['simulations', 'crop', 'indices']
+        args = path.split(str_fmt)
+        if len(args) != len(function_parameters):
+            raise TypeError(f"expected order is: {function_parameters}, crop, indices and simulations can be None"
+                            f"if replacement is related to soil properties, soil is a constant after the simulation name")
+        # bind them to the function paramters
+        fpv = dict(zip(function_parameters, args))
+
+        # by all means, we want indices to be evaluated
+
+        fpt = {k: (p := APSIMNG._try_literal_eval(v)) if (k in expected_nones) else (p := v)
+               for k, v in fpv.items()}
+        # we can now call the method below. First, we update param_values
+        fpt['param_values'] = param_values
+        return self.replace_soil_property_values(**fpt)
+
+    def update_mgt_by_path(self, *, path: str,
+                           param_values, fmt='.'):
+        """
+            Updates management parameters based on a given path and corresponding parameter values.
+
+            This method parses the provided `path` string, evaluates its components, and updates
+            management configurations using the `param_values`. The path is split using the specified
+            delimiter `fmt` and validated against a predefined guide of parameters. If the path is
+            invalid, a `ValueError` is raised.
+
+            Args: path (str): A formatted string that specifies the hierarchical path of parameters to update. The
+            default delimiter is a period ('.'). example: path should follow this order
+            'simulations_name.Manager.manager_name.out_path_name.parameter_name' Manager is constant, simulation_name
+            can be None the dfault simulations will be used, and the out_path can also be None the default will be
+            picked param_values: The value(s) to assign to the specified parameter in the management update.
+            fmt (str, optional): The delimiter used to split the path string. Default is '.'.
+
+            Raises:
+                ValueError: If the number of elements in the `path` does not match the expected structure.
+
+            Returns:
+                The result of the `update_mgt` function after applying the updates.
+
+            Example:
+                from apsimNGpy.core.base_data import load_default_simulations
+                model  = Replacements(load_default_simulations(crop ='maize').path)
+                path = "['Simulation'].Manager.Sow using a variable rule.None.Population"
+
+                path = "['Simulation'].Manager.Sow using a variable rule.None.Population"
+                param_values = 9
+                model.update_mgt_by_path(path=path, param_values=param_values, fmt = '.')
+                print(model.extract_user_input('Sow using a variable rule'))
+
+                 # if we want all simulations in the model to be changed, we keep that part None
+                example:
+                path = "None.Manager.Sow using a variable rule.None.Population"
+                param_values = 9
+                model.update_mgt_by_path(path=path, param_values=param_values, fmt = '.')
+                print(model.extract_user_input('Sow using a variable rule'))
+            """
         parameters_guide = ['simulations_name', 'Manager', 'manager_name', 'out_path_name', 'parameter_name']
         parameters = ['simulations', 'Manager', 'Name', 'out']
         args = path.split(fmt)
@@ -83,26 +187,6 @@ class Replacements(ReplacementHolder):
         _param_values = dict(zip(parameters, _eval_params))
 
         return self.update_mgt(**_param_values)
-
-    def update_child_params(self, child: str, **kwargs):
-        """Abstract method to perform various parameters replacements in apSim model. :param child: (str): name of
-        e.g., weather space is allowed for more descriptive one such a soil organic not case-sensitive :keyword
-        kwargs: these correspond to each node you are editing. Please see the corresponding methods for each node
-        the followingare the parameters for each node:
-        'cultivar': ('simulations','CultivarName', 'commands', 'values'),
-        'manager': ('management', 'simulations', 'out'),
-        'weather': ('weather_file', 'simulations'),
-        'soilphysical': ('parameter', 'param_values', 'simulation'),
-        'soilorganic': ('parameter', 'param_values', 'simulation'),
-        'soilchemical': ('parameter', 'param_values', 'simulation'),
-        'soilwater': ('parameter', 'param_values', 'simulation'),
-        'soilorganicmatter': ('simulations', 'inrm', 'icnr'),
-        'clock': ('start_date', 'end_date', 'simulations')
-        """
-        # Convert keys to lowercase
-        _child = child.lower().replace(" ", "")
-        # no need to filter paramters as it takes one child node
-        return self.replacement_methods(_child)(**kwargs)
 
     def update_children_params(self, children: tuple, **kwargs):
         """Method to perform various parameters replacements in apSim model.
@@ -149,7 +233,6 @@ if __name__ == '__main__':
 
     # the method make_replacements can be chained with several other action types
     mgt = {'Name': 'Sow using a variable rule', 'Population': 8.5},
-    model = ce.update_child_params(child='weather', weather_file=met)
     mgt = {'Name': 'Sow using a variable rule', 'Population': 7.5},
     chilredren = 'Manager', 'weather', 'SoilOrganicMatter'
     ce.update_children_params(children=chilredren, icnr=148, weather_file=met2, management=mgt)

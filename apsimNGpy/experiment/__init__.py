207eb2d900b4f1793c419e30f43d1aada431a712
MAGALA-RICHARD <rmagala640@gmail.com>
2024-09-16 12:25:52 -0500

    replace-soil_values_by_path

--- apsimNGpy/experiment/__init__.py
+++ apsimNGpy/experiment/__init__.py
@@ -1,5 +1,7 @@
+import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from experiment_utils import (_run_experiment, MetaInfo, copy_to_many, experiment_runner,
                               define_factor, Factor, define_cultivar)
+from main import Experiment
 
 __all__ = ['MetaInfo', 'copy_to_many', '_run_experiment', 'experiment_runner',
            'define_factor', 'Factor', 'define_cultivar']

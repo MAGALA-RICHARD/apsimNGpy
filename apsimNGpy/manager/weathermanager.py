15b735bb7d17dfc4def17af41218d1e7f4f4c951
MAGALA-RICHARD <rmagala640@gmail.com>
2024-09-20 22:07:56 -0500

    retry weather download after network failure

--- apsimNGpy/manager/weathermanager.py
+++ apsimNGpy/manager/weathermanager.py
@@ -355,28 +355,25 @@ def day_met_by_location(lonlat, start, end, cleanup=True, filename=None):
 
 
 def get_met_from_day_met(lonlat, start, end, filename=None, retry_number=1, **kwa):
-    """collect weather from daymet solar radiation is replaced with that of nasapower
+    """collect weather from daymet solar radiation is replaced with that of nasapower API
     ------------
     parameters
-    ---------------
-    retry_number
-    filename
-    start: Starting year
+    ---------------:
 
-    end: Ending year
-
-    lonlat: A tuple of xy cordnates
-
-    Cleanup:  A bolean True or False default is true: deletes the excel file generated during the file write up
-
-    ------------
-    returns a complete path to the new met file but also write the met file to the disk in the working directory
+    retry_number (int): retry number of times in case of netowrk errors
+    :filename. met file name to save on disk
+    start: Starting year of the met data
+    end: Ending year of the met data
+    lonlat (tuple): A tuple of XY cordnates, longitude first, then latitude second
     :keyword timeout specifies the waiting time
     :keyword wait: the time in secods to try for every retry in case of network errors
+    returns a complete path to the new met file but also write the met file to the disk in the working directory
+
     Example:
       # Assuming the function is imported:
           >>> from apsimNGpy.manager.weathermanager import get_met_from_day_met
-          >>> wf = get_met_from_day_met(lonlat=(-93.04, 42.01247), start=2000, end=2020, filename='daymet.met')
+          >>> wf = get_met_from_day_met(lonlat=(-93.04, 42.01247),
+              >>> start=2000, end=2020,timeout = 30, wait =2, retry_number=3, filename='daymet.met')
 
     """
     # import pdb

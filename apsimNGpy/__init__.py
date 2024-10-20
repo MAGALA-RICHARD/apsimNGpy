c78ae08c7320f1ac7fea668a858d4f3e367db02f
MAGALA-RICHARD <rmagala640@gmail.com>
2024-10-19 21:09:55 -0500

    no import of modules calling pythonnet api

--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -1,9 +1,13 @@
 
 try:
     from apsimNGpy import core, replacements, manager, utililies, config
+    from apsimNGpy.core.core import APSIMNG
+    from apsimNGpy.core.apsim import ApsimModel
 
-    __all__ = ['core', 'replacements', 'manager', 'utililies']
+    __all__ = ['core', 'replacements', 'manager',
+               'ApsimModel',
+               'utililies', 'config','APSIMNG']
 except Exception as e:
     # It's good practice to log the exception
-    print(f"An error occurred while importing: {e}")
+    ...
 

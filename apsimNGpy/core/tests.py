7b1250fa17222ed160f2969548e2db4a78743bea
richardmagala <rmagala640@gmail.com>
2024-10-25 18:34:30 -0500

    auto_detect

--- apsimNGpy/core/tests.py
+++ apsimNGpy/core/tests.py
@@ -7,7 +7,7 @@ current_path  = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_path)
 sys.path.append(os.path.dirname(current_path))
 from pythonet_config import GetAPSIMPath
-
+from core import APSIMNG
 # auto detect
 loaded = GetAPSIMPath()
 loaded.auto_detect()
@@ -24,3 +24,50 @@ if platform.system() == 'Darwin':
         print(path)
     if path[0]:
         Config.set_aPSim_bin_path(path)
+
+
+
+
+
+if __name__ == '__main__':
+
+    # test
+    from pathlib import Path
+    from time import perf_counter
+
+    # Model = FileFormat.ReadFromFile[Models.Core.Simulations](model, None, False)
+    os.chdir(Path.home())
+    from apsimNGpy.core.base_data import LoadExampleFiles, load_default_simulations
+
+    al = LoadExampleFiles(Path.cwd())
+    modelm = al.get_maize
+
+    model = load_default_simulations(crop ='maize')
+    for _ in range(1):
+
+        for rn in ['Maize, Soybean, Wheat', 'Maize', 'Soybean, Wheat']:
+            a = perf_counter()
+            # model.RevertCheckpoint()
+
+            print(model.extract_user_input('Simple Rotation'))
+            b = perf_counter()
+
+            print(b - a, 'seconds')
+            model.run('report')
+            print(model.results.mean(numeric_only=True))
+
+        a = perf_counter()
+
+        res = model.run_simulations(reports="Report", clean_up=False, results=True)
+        b = perf_counter()
+        print(b - a, 'seconds')
+        mod = model.Simulations
+        # xp = mod.FindAllInScope[Models.Manager]('Simple Rotation')
+        # a = [i for i in xp]
+        # for p in a:
+        #  for i in range(len(p.Parameters)):
+        #      kvp =p.Parameters[i]
+        #      if kvp.Key == "Crops":
+        #          updated_kvp = KeyValuePair[str, str](kvp.Key, "UpdatedValue")
+        #          p.Parameters[i] = updated_kvp
+        #      print(p.Parameters[i])

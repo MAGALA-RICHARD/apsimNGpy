098388d5a930f3f3465c728ae13c9c293c00e8e6
vanguard <rmagala640@gmail.com>
2024-10-28 06:15:45 -0500

    one

--- apsimNGpy/core/tests.py
+++ apsimNGpy/core/tests.py
@@ -12,7 +12,7 @@ user_id = current_path
 
 sys.path.append(current_path)
 sys.path.append(os.path.dirname(current_path))
-from path_finders import  auto_detect_apsim_bin_path
+from apsimNGpy.config import  auto_detect_apsim_bin_path
 from apsimNGpy.config import get_apsim_bin_path
 
 try:

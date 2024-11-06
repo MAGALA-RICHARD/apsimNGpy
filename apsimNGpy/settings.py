594d9106e81b2e8c4b5ad6ff579670158db3aefd
richardmagala <rmagala640@gmail.com>
2024-11-06 17:10:28 -0600

    progress bar

--- apsimNGpy/settings.py
+++ apsimNGpy/settings.py
@@ -8,7 +8,30 @@ WGS84 = 'epsg:4326'
 NUM_CORES: int = int(cpu_count() * 0.6)
 SOIL_THICKNESS: list = [150, 150, 200, 200, 200, 250, 300, 300, 400, 500]
 CRS: str = 'EPSG:26915'
+MSG  = """
+ERROR: APSIM Path Not Found
 
+It seems that the APSIM path is either not installed or not added to your environment variables.
+
+Please follow these steps to resolve the issue:
+
+1. **Install APSIM**:  
+   - If you haven't installed APSIM yet, you can download it here:  
+     [APSIM Registration Page](https://registration.apsim.info/)
+   - Follow the installation guidelines here:  
+     [APSIM Installation Guide](https://apsimnextgeneration.netlify.app/install/)
+
+2. **Access APSIM Tutorials**:  
+   - For getting started with APSIM, check out the user tutorials:  
+     [APSIM User Tutorials](https://apsimnextgeneration.netlify.app/user_tutorials/)
+
+3. **Set APSIM Bin Path**:  
+   After installing APSIM, make sure to add the installation path to your environment variables. You can do this by using the following Python command:
+
+   ```python
+   from apsimNGpy.config import set_apsim_bin_path
+   set_apsim_bin_path('~/your/path/to/bin')
+"""
 # configure the logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)

24f57a7fb06ffebf3f5590fc2e9254c208b39f27
richardmagala <rmagala640@gmail.com>
2024-11-10 12:33:50 -0600

    get_p

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
-from settings import MSG, logger
-from config import get_apsim_bin_path,  create_config, set_apsim_bin_path
 
-configured = get_apsim_bin_path() or ''
-if not configured:
-  logger.debug(MSG)
+from config import get_apsim_bin_path, set_apsim_bin_path
 
-in_modules =  ['get_apsim_bin_path',  set_apsim_bin_path, 'auto_detect_apsim_bin_path']
+configured = get_apsim_bin_path()
+
+in_modules =  ['get_apsim_bin_path',  'set_apsim_bin_path', 'auto_detect_apsim_bin_path']
 
 try:
     from apsimNGpy import core, replacements, manager, utililies

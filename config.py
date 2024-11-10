4174447a33ea8392fa19d1cd025b045180ba8ff6
richardmagala <rmagala640@gmail.com>
2024-11-09 23:15:00 -0600

    get_p

--- config.py
+++ config.py
@@ -71,9 +71,11 @@ def auto_detect_apsim_bin_path():
     if platform.system() == 'Windows':
         return os.getenv("APSIM") or os.getenv("Models") or search_from_programs() or search_from_users() or ""
     if platform.system() == 'Darwin':
-        # we search in applications and give up
+        # we search in applications and home and give up
         pattern = '/Applications/APSIM*.app/Contents/Resources/bin'
-        return _match_pattern_to_path(pattern) or ""
+        _home  =os.path.expanduser('~')
+        pattern2 = f"{_home}/APSIM*.app/Contents/Resources/bin"
+        return _match_pattern_to_path(pattern) or _match_pattern_to_path(pattern2)  or ""
 
     if platform.system() == 'Linux':
         pattern1 = '/usr/local/APSIM*/Contents/Resources/bin'

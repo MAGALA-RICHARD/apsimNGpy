f00d5c62099127697a012891918fe08f1636e9a6
richardmagala <rmagala640@gmail.com>
2024-11-10 21:33:47 -0600

    get_p

--- __init__.py
+++ __init__.py
@@ -1,20 +1,8 @@
 
-from config import get_apsim_bin_path, set_apsim_bin_path
+from config import get_apsim_bin_path, set_apsim_bin_path, auto_detect_apsim_bin_path
 
 
 in_modules =  ['get_apsim_bin_path',  'set_apsim_bin_path', 'auto_detect_apsim_bin_path']
 
-try:
-    from apsimNGpy import core, replacements, manager, utililies
-    from apsimNGpy.core.core import APSIMNG
-    from apsimNGpy.core.apsim import ApsimModel
-
-    in_modules.extend(['core', 'replacements', 'manager',
-                       'ApsimModel',
-                       'utililies', 'config', 'APSIMNG'])
-
-except Exception as e:
-
-    pass
 
 __all__ = in_modules
--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -1,6 +1,5 @@
-from config import set_apsim_bin_path, get_apsim_bin_path
 
-in_modules = ['get_apsim_bin_path', 'set_apsim_bin_path']
+in_modules = []
 
 try:
     from apsimNGpy import core, replacements, manager, utililies

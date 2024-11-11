ca6eaf3abb85e9087f57a5e1d0294fe647f378b9
richardmagala <rmagala640@gmail.com>
2024-11-10 21:30:42 -0600

    get_p

--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -1,15 +1,4 @@
-import warnings
-from apsimNGpy.settings import  MSG, logger
-
-from apsimNGpy.config import get_apsim_bin_path, auto_detect_apsim_bin_path, create_config, set_apsim_bin_path
-
-configured = get_apsim_bin_path() or auto_detect_apsim_bin_path() or ''
-if not configured:
-    logger.debug(MSG)
-else:
-    # if the above is false, it implies config.ini is nit created, we created it and insert ''
-    create_config(apsim_path=configured)
-
+from config import set_apsim_bin_path, get_apsim_bin_path
 
 in_modules = ['get_apsim_bin_path', 'set_apsim_bin_path']
 

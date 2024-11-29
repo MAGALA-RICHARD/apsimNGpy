558919d5879e08aa11904e66c590dc303d6c0150
MAGALA-RICHARD <rmagala640@gmail.com>
2024-11-29 16:44:40 -0600

    core doc

--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -1,16 +1,17 @@
 import os.path
 import sys
+
 sys.path.append(os.path.realpath(".."))
-import config
-in_modules = ['config']
+
+in_modules = []
 try:
     from apsimNGpy import core, replacements, manager, utililies
     from apsimNGpy.core.core import APSIMNG
     from apsimNGpy.core.apsim import ApsimModel
 
     in_modules.extend(['core', 'replacements', 'manager',
-                  'ApsimModel',
-                  'utililies', 'config', 'APSIMNG'])
+                       'ApsimModel',
+                       'utililies', 'config', 'APSIMNG'])
 
 except Exception as e:
 

a67c615f0b22fa3ffbfca78982944940c4af6356
MAGALA-RICHARD <rmagala640@gmail.com>
2024-12-02 12:57:49 -0600

    core doc

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from config import get_apsim_bin_path, set_apsim_bin_path, auto_detect_apsim_bin_path
-import config
-__all__ = ['get_apsim_bin_path','config', 'set_apsim_bin_path', 'auto_detect_apsim_bin_path']

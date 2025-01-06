f13be80948c556c6b12c52c39464d52d3b99761d
rmagala <rmagala640@gmail.com>
2025-01-06 08:21:38 -0600

    optimize

--- apsimNGpy/optimizer/optimizer.py
+++ apsimNGpy/optimizer/optimizer.py
@@ -1,19 +1,20 @@
-from builtins import function
+from copy import deepcopy
 
 import pandas as pd
 import time
+
+from attr import dataclass
+from past.builtins import reload
 from scipy.optimize import minimize, least_squares
 from apsimNGpy.utililies.utils import delete_simulation_files, timer
 
 import numpy as np
 from apsimNGpy.core.apsim import ApsimModel
+from apsimNGpy.replacements.replacements import Replacements
 from enum import Enum
 
-df = pd.read_csv("simu_rn.csv")
-residue = list(df.residue.unique())
-nitrogen = list(df.nitrogen.unique())
-m = 0
 from abc import ABC, abstractmethod
+
 
 
 class AbstractExecutor(ABC):
@@ -45,11 +46,8 @@ executor = FunctionExecutor(sample_function)
 result = executor.execute(5, 3)
 print(result)  # Output: 8
 
-
-
-
-
-class algarithms(Enum):
+@dataclass
+class Solvers:
     L_BFGS_B = 'L-BFGS-B'
     BFGS = 'BFGS'
     Powell = 'Powell'
@@ -57,9 +55,38 @@ class algarithms(Enum):
     lm = 'lm'
 
 
+class Problem(ApsimModel):
+
+    def __init__(self, model, out_path = None, bounds =None):
+        super().__init__(model, out_path)
+        """
+        APSIM model file, apsimNGpy object, apsim file str or dict we want to use in the minimization
+        """
+        # wrap the replacement methods here for easy access in the execution function below
+        self.bounds = bounds # note that the bounds is a tuple for each variable we want to optimize
+
+    def function(self, *args):
+        """
+        Ths class is  holder for the problem function
+         use the replacement module to define the functional call for the methods
+         once initiated, we have access to all the methods from the replacements module
+         args must  correspond to the bounds deployed while initializing this class
+        """
+        pass
+
+
+def minimize_problem(problem,**kwargs):
+    """problem class inherited from Problem class
+    kwargs: key word arguments as defined by the scipy minimize method
+    """
+    if 'bounds' in kwargs:
+         kwargs.pop('bounds')
+    return minimize(problem.function, bounds= problem.bounds,
+                    **kwargs)
+
 @timer
-class Optimize():
-    def __init__(self, func: function, init_params: np.ndarray, bounds: list, options: dict = None, method='L-BFGS-B',
+class Minimize():
+    def __init__(self, func, init_params: tuple, bounds: tuple, options: dict = None, method='L-BFGS-B',
                  *args):
 
         self.method = method
@@ -73,4 +100,28 @@ class Optimize():
         optimized_parameters = self.result.x
         print("Optimized Parameters:", optimized_parameters)
 
+if __name__ == '__main__':
+    # define the problem
+    from apsimNGpy.core.base_data import load_default_simulations
 
+    class MyProblem(Problem):
+        def __init__(self, model):
+              super().__init__(model)
+              self.bounds = [(0, 100)]
+
+        def function(self, value):
+
+            #mgt ='None.Manager.Fertilise at sowing.e.Amount'
+            mgt = ({"Name": 'Fertilise at sowing', "Amount":value},)
+            self.update_mgt(simulations= ['Simulation'], management=mgt,out ='op.apsimx')
+            self.run(report_name='Report')
+            df = self.results
+
+            ans = df.Yield.mean()
+            print(ans)
+            return ans * -1
+
+
+    maize = load_default_simulations(crop='maize', simulations_object=False)
+    prob = MyProblem(maize)
+    res = minimize_problem(prob, x0 =[0], method='L-BFGS-B')

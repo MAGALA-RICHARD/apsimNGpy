abb45f8c1bd489358c91169c500a1e809e7361dd
rmagala <rmagala640@gmail.com>
2025-02-14 13:43:43 -0600

    updator

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'commands=apsimNGpy.cli:main',
-            'defaults=apsimNGpy.default:main'
+            'commands=apsimNGpy.cli.cli:main',
+            'defaults=apsimNGpy.cli.default:main'
         ],
     },
 

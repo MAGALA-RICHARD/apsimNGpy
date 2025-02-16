1c858e1267fc3ef3546efef78efb3bfde781e13c
rmagala <rmagala640@gmail.com>
2025-02-15 22:01:33 -0600

    cli diagnostic

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-VERSION = '0.1.1.0'
+VERSION = '0.28.0'
 DESCRIPTION = 'apsimx next generation package interface'
 LONG_DESCRIPTION = 'run, edit, download soils and weather and interact with the apsimx file'
 

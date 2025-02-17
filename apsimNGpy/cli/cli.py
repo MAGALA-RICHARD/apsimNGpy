7c40161b6b824add023fb92225aa45b91435d200
richard magala <rmagala640@gmail.com>
2025-02-16 22:15:46 -0600

    cli diagnostic

--- apsimNGpy/cli/cli.py
+++ apsimNGpy/cli/cli.py
@@ -1,5 +1,7 @@
 import argparse
 import json
+import os.path
+
 from apsimNGpy.core.apsim import ApsimModel
 from apsimNGpy.core.base_data import load_default_simulations
 from apsimNGpy.settings import logger
@@ -10,17 +12,28 @@ def main():
     parser = argparse.ArgumentParser(description='Run a simulation of a given crop.')
 
     # Add arguments
-    parser.add_argument('-m', '--model', type=str, required=True, help='Path to the APSIM model file')
+    parser.add_argument('-m', '--model', type=str, required=False, help='Path to the APSIM model file', default ='Maize')
     parser.add_argument('-o', '--out', type=str, required=False,
                         help='out path')
-    parser.add_argument('-t', '--table', type=str, required=False)
+    parser.add_argument('-t', '--table', type=str, required=False, default='Report')
+    parser.add_argument('-w', '--met_file', type=str, required=False)
+    parser.add_argument('-sim', '--simulation', type=str, required=False)
+    parser.add_argument('-ws', '--wd', type=str, required=False)
+
 
     # Parse arguments
     args = parser.parse_args()
 
-    # Run the simulation using the provided model and location
-    model = ApsimModel(args.model, out=args.out)
-
+    wd = args.wd or os.getcwd()
+    if wd != os.getcwd():
+        os.makedirs(wd, exist_ok=True)
+    if args.model.endswith('.apsimx'):
+        model = ApsimModel(args.model)
+    else:
+        model = load_default_simulations(crop=args.model, simulations_object=True, path = wd)
+    if args.met_file is not None:
+        model.replace_met_file(weather_file=args.met_file, simulations=args.simulation)
+        print(f'successfully updated weather file with {args.met_file}')
     model.run(report_name=args.table)
     print(model.results)
 
@@ -31,7 +44,7 @@ def defaults_model():
     # Create argument parser
     parser = argparse.ArgumentParser(description='Run a simulation of a given crop.')
     # Add arguments
-    parser.add_argument('-c', '--crop', type=str, required=False, help='name of the crop', default='Maize')
+    parser.add_argument('-c', '--model', type=str, required=False, help='name of the model')
     parser.add_argument('-o', '--out', type=str, required=False,
                         help='out path for the new model')
     parser.add_argument('-t', '--table', type=str, required=False, default='Report', help='table or report name in '
@@ -39,13 +52,13 @@ def defaults_model():
     parser.add_argument('-st', '--saveto', type=str, required=False, default=None,
                         help='name to save the results')
 
+    parser.add_argument('-w', '--met_file', type=str, required=False)
+    parser.add_argument('-sim', '--simulation', type=str, required=False)
+
     # Parse arguments
     args = parser.parse_args()
 
-    # Run the simulation using the provided model and location
-    model_file = args.crop
-
-    model_file = load_default_simulations(crop=args.crop, simulations_object=False)
+    model_file = load_default_simulations(crop=args.model, simulations_object=False, path = os.getcwd())
     model = ApsimModel(model_file)
     logger.info(f'RUNNING: {model}')
     table_name = args.table
@@ -54,9 +67,13 @@ def defaults_model():
     if args.saveto is not None:
         res.to_csv(args.saveto)
         logger.info(f"results saved to {args.saveto}")
+    if args.met_file is not None:
+        model.replace_met_file(weather_file=args.met_file, simulations=args.simulation)
     logger.info(res.describe())
 
 
 if __name__ == "__main__":
-    defaults_model()
+    ...
+    main()
+
 

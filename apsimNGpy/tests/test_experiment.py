70836dc1d73780fdb4679c64b5e804b50c0d3ab7
richardmagala <rmagala640@gmail.com>
2025-04-20 19:58:53 -0500

    config_ini

--- apsimNGpy/tests/test_experiment.py
+++ apsimNGpy/tests/test_experiment.py
@@ -1,23 +1,30 @@
 import unittest
 from pandas import DataFrame
-from apsimNGpy.tests.base_test import BaseTester, os
+from apsimNGpy.tests.base_test import BaseTester, set_wd
+
+set_wd()
 
 
 class TestExperiment(BaseTester):
 
-    def test_experiment(self):
+    def test_add_factor_with_permutes(self):
+        self.test_ap_sim.create_experiment(permutation=True)
         # experiment is initialized in base_tester class SetUp
-        file = self.experiment.path
-        self.assertTrue(os.path.exists(file))
-
-    def test_add_factor(self):
-        # experiment is initialized in base_tester class SetUp
-        self.experiment.add_factor(specification="[Fertilise at sowing].Script.Amount = 0 to 200 step 20",
+        self.test_ap_sim.add_factor(specification="[Fertilise at sowing].Script.Amount = 0 to 200 step 50",
                                    factor_name='Nitrogen')
-        self.experiment.run()
-        self.assertIsInstance(self.experiment.results, DataFrame)
-        if isinstance(self.experiment.results, DataFrame):
-            self.assertFalse(self.experiment.results.empty, msg='results are empty')
+        self.test_ap_sim.run()
+        self.assertIsInstance(self.test_ap_sim.results, DataFrame)
+        if isinstance(self.test_ap_sim.results, DataFrame):
+            self.assertFalse(self.test_ap_sim.results.empty, msg='results are empty')
+    def test_add_factor_without_permutes(self):
+        self.test_ap_sim.create_experiment(permutation=False)
+        # experiment is initialized in base_tester class SetUp
+        self.test_ap_sim.add_factor(specification="[Fertilise at sowing].Script.Amount = 0 to 200 step 50",
+                                   factor_name='Nitrogen')
+        self.test_ap_sim.run()
+        self.assertIsInstance(self.test_ap_sim.results, DataFrame)
+        if isinstance(self.test_ap_sim.results, DataFrame):
+            self.assertFalse(self.test_ap_sim.results.empty, msg='results are empty')
 
 
 if __name__ == '__main__':

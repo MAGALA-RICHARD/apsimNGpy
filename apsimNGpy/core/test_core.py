2627467b50bcc5a4563039f01b7214401b35e5ea
richard magala <rmagala640@gmail.com>
2025-05-23 13:48:52 -0500

    removed_get_reports

---
+++ apsimNGpy/core/test_core.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestCoreModel(TestCase):
+    def test_check_model(self):
+        self.fail()
+
+    def test__remove_related_files(self):
+        self.fail()
+
+    def test_simulations(self):
+        self.fail()
+
+    def test_simulation_names(self):
+        self.fail()
+
+    def test_run(self):
+        self.fail()
+
+    def test_simulated_results(self):
+        self.fail()

40b5a87a32b5645cea10e809a4701f2030e308c9
richard magala <rmagala640@gmail.com>
2025-06-15 08:40:46 -0500

    test edit-cultivar

--- apsimNGpy/w/cleaner.py
+++ apsimNGpy/w/cleaner.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import os
 import schedule
 import time
+
+
 def clean():
     path = Path("../../")
     ax = path.rglob('*.apsimx')
@@ -13,8 +15,9 @@ def clean():
             os.remove(str(i))
         except PermissionError:
             print(f"skipping '{i}' due to permission error")
-    if fl >0:
-      print(f" cleaned {fl} unwanted files")
+    if fl > 0:
+        print(f" cleaned {fl} unwanted files")
+
 
 if __name__ == "__main__":
     def job_with_argument(name):

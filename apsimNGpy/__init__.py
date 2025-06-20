4ec070a05e4f156eee0b0871e378ba069f349e4e
vanguard <rmagala640@gmail.com>
2025-06-20 12:03:25 -0500

    diag

--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -1,11 +1,13 @@
 from apsimNGpy.core.config import get_apsim_bin_path, set_apsim_bin_path, auto_detect_apsim_bin_path
 __version__ = '0.39.3.2'
+
 __all__ = ['get_apsim_bin_path', 'auto_detect_apsim_bin_path', 'set_apsim_bin_path', '__version__']
 
 if get_apsim_bin_path():
 
     # import apsimNgpy objects
     from apsimNGpy.core import core, apsim, base_data
+    from apsimNGpy.core.apsim import ApsimModel
     from apsimNGpy.optimizer import mixed, one_objective, one_obj
     from apsimNGpy.validation import evaluator, eval_methods
     from apsimNGpy.parallel.process import custom_parallel, run_apsimx_files_in_parallel, _read_result_in_parallel
@@ -13,7 +15,7 @@ if get_apsim_bin_path():
     from apsimNGpy.manager.weathermanager import get_met_from_day_met, get_met_nasa_power, get_iem_by_station, get_weather
 
 
-    __all__.extend(['core', 'apsim', 'get_apsim_bin_path', 'base_data',
+    __all__.extend(['core', 'apsim', 'get_apsim_bin_path', 'base_data','ApsimModel',
                     'mixed', 'one_objective', 'one_obj',
                     'evaluator', 'eval_methods',
                         "custom_parallel", 'run_apsimx_files_in_parallel', '_read_result_in_parallel',

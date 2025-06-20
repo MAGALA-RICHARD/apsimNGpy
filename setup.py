d5ae48222f48312067b4c0c36cda78f3750e7e4d
vanguard <rmagala640@gmail.com>
2025-06-20 10:55:21 -0500

    diag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-VERSION = '0.41'
+VERSION = '0.39.3.2'
 
 DESCRIPTION = 'APSIM next generation package interface'
 LONG_DESCRIPTION = 'Run, edit, download soils and weather and interact with the apsimx file'

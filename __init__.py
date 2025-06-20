34d2cbf1cddb6c405efed54adee5124a9fbcb324
vanguard <rmagala640@gmail.com>
2025-06-20 11:05:51 -0500

    diag

--- __init__.py
+++ __init__.py
@@ -5,9 +5,19 @@ if get_apsim_bin_path():
     # import apsimNgpy objects
     from apsimNGpy.core import core, apsim, base_data
     from apsimNGpy.optimizer import mixed, one_objective, one_obj
+    from apsimNGpy.validation import evaluator, eval_methods
+    from apsimNGpy.parallel.process import custom_parallel, run_apsimx_files_in_parallel, _read_result_in_parallel
+    from apsimNGpy.core_utils import database_utils
+    from apsimNGpy.manager.weathermanager import get_met_from_day_met, get_met_nasa_power, get_iem_by_station, get_weather
+
+
 
     __all__.extend(['core', 'apsim', 'get_apsim_bin_path', 'base_data',
-                    'mixed', 'one_objective', 'one_obj,variables'])
+                    'mixed', 'one_objective', 'one_obj',
+                    'evaluator', 'eval_methods',
+                        "custom_parallel", 'run_apsimx_files_in_parallel', '_read_result_in_parallel',
+                    'database_utils',
+                   " get_met_from_day_met", "get_met_nasa_power", "get_iem_by_station", "get_weather"])
 
 
 # Define the apsimNgpy version

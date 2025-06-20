7c66cb20aa6bd3a5ceb79aae59e53d8aa3e230bd
vanguard <rmagala640@gmail.com>
2025-06-20 11:47:50 -0500

    diag

--- apsimNGpy/__init__.py
+++ apsimNGpy/__init__.py
@@ -1,7 +1,9 @@
 from apsimNGpy.core.config import get_apsim_bin_path, set_apsim_bin_path, auto_detect_apsim_bin_path
 
 __all__ = ['get_apsim_bin_path', 'auto_detect_apsim_bin_path', 'set_apsim_bin_path']
+
 if get_apsim_bin_path():
+
     # import apsimNgpy objects
     from apsimNGpy.core import core, apsim, base_data
     from apsimNGpy.optimizer import mixed, one_objective, one_obj

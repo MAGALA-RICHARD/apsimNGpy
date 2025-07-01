c48a3a60f6b7b54a5f19c3b9094c26f2b36987d0
richard magala <rmagala640@gmail.com>
2025-07-01 14:54:26 -0500

    optimization 90% complete

--- apsimNGpy/tests/test_moo_whole.py
+++ apsimNGpy/tests/test_moo_whole.py
@@ -1,6 +1,7 @@
-from apsimNGpy.optimizer.moo import ApsimOptimizationProblem, Runner, compute_hyper_volume, NSGA2
+from apsimNGpy.optimizer.moo import MultiObjectiveProblem, compute_hyper_volume, NSGA2
 from pymoo.optimize import minimize
 import matplotlib.pyplot as plt
+from apsimNGpy.core.apsim import ApsimModel as Runner
 import numpy as np
 # 1. Setup APSIM model
 runner = Runner("Maize")
@@ -29,11 +30,11 @@ def nitrate_leaching(df):
 objectives = [maximize_yield, nitrate_leaching]
 
 
-# prob = ApsimOptimizationProblem(runner, _vars, objectives)
+# prob = MultiObjectiveProblem(runner, _vars, objectives)
 
 def run_example():
     # 4. Define problem and optimizer
-    problem = ApsimOptimizationProblem(runner, objectives=objectives, decision_vars=_vars)
+    problem = MultiObjectiveProblem(runner, objectives=objectives, decision_vars=_vars)
     # problem.add_control(
     #     **{'path': '.Simulations.Simulation.Field.Fertilise at sowing', 'Amount': "?", "bounds": [50, 300],
     #        "v_type": "float"})
@@ -65,3 +66,45 @@ def run_example():
 
 if __name__ == '__main__':
     res= run_example()
+    import os
+
+    from sklearn.preprocessing import MinMaxScaler
+
+    scaler = MinMaxScaler()
+    f_scaled = scaler.fit_transform(res.F)
+
+    import numpy as np
+    from pymoo.visualization.scatter import Scatter
+    from pymoo.mcdm.high_tradeoff import HighTradeoffPoints
+
+    pf = res.F
+    dm = HighTradeoffPoints()
+
+    I = dm(f_scaled)
+    tdp = f_scaled[I]
+
+
+    plt.scatter(f_scaled[:, 0], f_scaled[:, 1])
+    plt.scatter(tdp[0][0] * -1, tdp[0][0], c="red", s=100, label='Selected Solution')
+    plt.xlabel("Yield")
+    plt.ylabel("N Leaching")
+    plt.title("Pareto Front")
+    plt.show()
+
+    weights = 1/np.array([0.6, 0.4])
+
+    def compromise_with_weights(res, weights):
+        assert np.sum(weights) == 1, "weights must sum to 1"
+        from pymoo.decomposition.asf import ASF
+        scaler = MinMaxScaler()
+        decomp = ASF()
+        nF = scaler.fit_transform(res.F)
+        i = decomp.do(nF, 1 / weights).argmin()
+        # plot
+        plt.scatter(res.F[:, 0], res.F[:, 1], c='blue')
+        plt.scatter(res.F[i, 0], res.F[i, 1], c="red", s=100, label='Selected Solution')
+        plt.xlabel("Yield")
+        plt.ylabel("N Leaching")
+        plt.title("Pareto Front")
+        plt.show()
+        return res.X[i, 0], res.X[i, 1]

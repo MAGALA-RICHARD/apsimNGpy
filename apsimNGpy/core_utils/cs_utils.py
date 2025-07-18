dc883cb2e05d0694c728ec13698d24d2f46be6a5
richard magala <rmagala640@gmail.com>
2025-07-18 11:07:57 -0500

    cs cast

---
+++ apsimNGpy/core_utils/cs_utils.py
@@ -0,0 +1,8 @@
+def start_pythonnet():
+    import pythonnet
+    try:
+        if pythonnet.get_runtime_info() is None:
+            return pythonnet.load("coreclr")
+    except:
+        print("dotnet not found, trying alternate runtime")
+        return pythonnet.load()

375fad5bf929fe03c43ba7b2710bb566080d101d
richard magala <rmagala640@gmail.com>
2025-08-08 09:11:44 -0500

    new

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import sys
 from functools import cache
 
-VERSION = '0.39.3.8'
+VERSION = '0.39.3.9'
 
 DESCRIPTION = 'APSIM next generation package interface'
 LONG_DESCRIPTION = 'Run, edit, download soils and weather and interact with the apsimx file'

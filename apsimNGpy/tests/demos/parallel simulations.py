8f750ce4cd71d34de2be139456a99d957ffdc74b
richard magala <rmagala640@gmail.com>
2025-08-13 13:11:08 -0500

    optimizations

--- apsimNGpy/tests/demos/parallel simulations.py
+++ apsimNGpy/tests/demos/parallel simulations.py
@@ -3,7 +3,6 @@ if True:
     from apsimNGpy.core.marathon import ParallelRunner
     from apsimNGpy.core.apsim import ApsimModel
     from pathlib import Path
-    import shutil
 
     if __name__ == "__main__":
         create_jobs = [ApsimModel('Maize').path for _ in range(1000)] # substitute with real simulation tasks
@@ -11,11 +10,7 @@ if True:
         Parallel.run_all_jobs(create_jobs, n_cores=16, threads=False, clear_db=True)
         df = Parallel.get_simulated_output(axis=0)
         wdr = Path(".").glob("*scratch")
-        try:
-            [shutil.rmtree(i) for i in wdr]
-            print("Deleted scratch work space")
-        except PermissionError:
-            pass
+
 
 
 

76b9a3caf40ca6d8b08916190e34f073cf19653b
richard magala <rmagala640@gmail.com>
2025-08-17 14:19:29 -0500

    change the order of file name evaluation get_weather_from_web

--- apsimNGpy/tests/tester_main.py
+++ apsimNGpy/tests/tester_main.py
@@ -97,6 +97,8 @@ modules = [m for m in (model_tools,
                        test_model_loader,
                        test_config,
                        test_weathermanager,
+
+                       test_multcores,
                        test_get_weather_from_web_filename,
 
                        )]

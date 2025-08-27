501cd6ad35493a6e41d25b5aefe58cff74d33331
magala-richard <rmagala640@gmail.com>
2025-08-26 20:16:41 -0500

    patches to support latest APSIM file structure

--- apsimNGpy/tests/unittests/test_experiment.py
+++ apsimNGpy/tests/unittests/test_experiment.py
@@ -9,9 +9,12 @@ cwd = Path.cwd()
 
 
 class TestExperiment(unittest.TestCase):
+    def setUp(self):
+        self.experiment_path = Path(f"{self._testMethodName}.apsimx")
+        self.save_file_name = Path(f"{self._testMethodName}.apsimx")
 
     def test_add_factor_with_permutes(self):
-        t_experiment = Experiment('Maize', out=cwd.joinpath('my_experiment.apsimx'))
+        t_experiment = Experiment('Maize', out_path=self.experiment_path)
         t_experiment.init_experiment(permutation=True)
         # experiment is initialized in base_tester class SetUp
         t_experiment.add_factor(specification="[Fertilise at sowing].Script.Amount = 0 to 200 step 50",
@@ -22,7 +25,7 @@ class TestExperiment(unittest.TestCase):
             self.assertFalse(t_experiment.results.empty, msg='results are empty')
 
     def test_add_factor_with_permutes_2_factors(self):
-        t_experiment = Experiment('Maize', out=cwd.joinpath('my_experiment2.apsimx'))
+        t_experiment = Experiment('Maize', out_path=self.experiment_path)
         t_experiment.init_experiment(permutation=True)
         # experiment is initialized in base_tester class SetUp
         t_experiment.add_factor(specification="[Fertilise at sowing].Script.Amount = 0 to 200 step 50",
@@ -37,7 +40,7 @@ class TestExperiment(unittest.TestCase):
 
     def test_inspection(self):
 
-        t_experiment = Experiment('Maize', out=cwd.joinpath('my_experiment2.apsimx'))
+        t_experiment = Experiment('Maize', out_path=self.experiment_path)
         t_experiment.init_experiment(permutation=True)
         # experiment is initialized in base_tester class SetUp
         t_experiment.add_factor(specification="[Fertilise at sowing].Script.Amount = 0 to 200 step 50",
@@ -53,6 +56,8 @@ class TestExperiment(unittest.TestCase):
         self.assertTrue(managers, msg='managers not found in the experiment')
 
     def test_save(self):
+        self.experiment_path.unlink(missing_ok=True)
+        self.save_file_name.unlink(missing_ok=True)
         t_experiment = Experiment('Maize', out=os.path.realpath('my_experiment3.apsimx'))
         t_experiment.init_experiment(permutation=True)
         # experiment is initialized in base_tester class SetUp
@@ -61,20 +66,24 @@ class TestExperiment(unittest.TestCase):
         t_experiment.add_factor(specification="[Sow using a variable rule].Script.Population =4 to 8 step 2",
                                 factor_name='Population')
         filename = str(cwd.joinpath('my_experiment_saved.apsimx'))
-        t_experiment.save(file_name=filename)
+        t_experiment.save(file_name=str(self.save_file_name))
         self.assertTrue(os.path.exists(filename), msg='saving experiment failed')
+        self.assertGreater(self.experiment_path.stat().st_size, 0, msg='experiment not successfully saved')
+        self.assertGreater(self.save_file_name.stat().st_size, 0, msg='experiment not successfully saved')
 
     def test_save_add_factor(self):
-        t_experiment = Experiment('Maize', out=os.path.realpath('my_experiment5.apsimx'))
+
+        self.save_file_name.unlink(missing_ok=True)
+        t_experiment = Experiment('Maize', out_path=self.experiment_path)
         t_experiment.init_experiment(permutation=True)
         # experiment is initialized in base_tester class SetUp
         t_experiment.add_factor(specification="[Fertilise at sowing].Script.Amount = 0 to 200 step 50",
                                 factor_name='Nitrogen')
         t_experiment.add_factor(specification="[Sow using a variable rule].Script.Population =4 to 8 step 2",
                                 factor_name='Population')
-        filename = str(cwd.joinpath('my_experiment_saved2.apsimx'))
-        t_experiment.save(file_name=filename)
-        self.assertTrue(os.path.exists(filename), msg='saving experiment failed')
+
+        t_experiment.save(file_name=str(self.save_file_name))
+        self.assertGreater(self.save_file_name.stat().st_size, 0, msg='experiment not successfully saved')
         try:
             error = False
             t_experiment.add_factor(specification="[Sow using a variable rule].Script.Population =4 to 8 step 2",
@@ -83,13 +92,18 @@ class TestExperiment(unittest.TestCase):
             print(e)
             error = True
         self.assertFalse(error, 'saving and adding factor failed')
+        # save again
+        self.save_file_name.unlink(missing_ok=True)
+        t_experiment.save(file_name=str(self.save_file_name))
+        self.assertGreater(self.save_file_name.stat().st_size, 0,
+                           msg='experiment not successfully saved after duplicate factor addition test')
 
     def test_add_factor_with_permutes_fct_rep(self):
         """
         Test that we can handle the unmistakable addition of factors which are identical
         @return:
         """
-        t_experiment = Experiment('Maize', out=cwd.joinpath('my_experiment33.apsimx'))
+        t_experiment = Experiment('Maize', out_path=self.experiment_path)
         t_experiment.init_experiment(permutation=True)
         # experiment is initialized in base_tester class SetUp
         t_experiment.add_factor(specification="[Fertilise at sowing].Script.Amount = 0 to 200 step 50",
@@ -109,7 +123,7 @@ class TestExperiment(unittest.TestCase):
         self.assertEqual(t_experiment.n_factors, 2, msg='n_factors is greater than 3  factor repetition tests failed')
 
     def test_add_factor_without_permutes(self):
-        t_experiment = Experiment('Maize', out=cwd.joinpath('my_experiment3.apsimx'))
+        t_experiment = Experiment('Maize', out_path=self.experiment_path)
         t_experiment.init_experiment(permutation=False)
         # experiment is initialized in base_tester class SetUp
         t_experiment.add_factor(specification="[Fertilise at sowing].Script.Amount = 0 to 200 step 50",
@@ -118,6 +132,9 @@ class TestExperiment(unittest.TestCase):
         self.assertIsInstance(t_experiment.results, DataFrame)
         if isinstance(t_experiment.results, DataFrame):
             self.assertFalse(t_experiment.results.empty, msg='results are empty')
+
+    def tearDown(self):
+        self.experiment_path.unlink(missing_ok=True)
 
 
 if __name__ == '__main__':

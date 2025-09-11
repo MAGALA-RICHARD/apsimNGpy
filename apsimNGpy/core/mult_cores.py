df409cab280ea8c390ebdbe7f6d1f055a6292dac
magala-richard <rmagala640@gmail.com>
2025-09-11 15:07:51 -0500

    support building from source

--- apsimNGpy/core/mult_cores.py
+++ apsimNGpy/core/mult_cores.py
@@ -255,10 +255,11 @@ class MultiCoreManager:
 
 if __name__ == '__main__':
     # quick tests
-    create_jobs = (ApsimModel('Maize').path for _ in range(3000))
+    create_jobs = (ApsimModel('Maize').path for _ in range(300))
 
     Parallel = MultiCoreManager(db_path='testing.db', agg_func=None)
-    Parallel.run_all_jobs(create_jobs, n_cores=4, threads=False, clear_db=True, clean_up=True)
+    Parallel.run_all_jobs(create_jobs, n_cores=4, threads=False, clear_db=True,)
     df = Parallel.get_simulated_output(axis=0)
+    Parallel.clear_scratch()
 
     insert_data_with_pd('tss.db', 'my', df, 'replace')

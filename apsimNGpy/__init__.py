c51f5957ad9d48ae6e814f9471da66004665b6cc
vanguard <rmagala640@gmail.com>
2025-09-12 20:18:12 -0500

    strict evaluation in loading pythonnet

---
+++ apsimNGpy/__init__.py
@@ -0,0 +1,5 @@
+from apsimNGpy.core.config import set_apsim_bin_path, get_apsim_bin_path, locate_model_bin_path
+
+__all__ = [set_apsim_bin_path, get_apsim_bin_path, locate_model_bin_path]
+
+CURRENT_BIN = get_apsim_bin_path()

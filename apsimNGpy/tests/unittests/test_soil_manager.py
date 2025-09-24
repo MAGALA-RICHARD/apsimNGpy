eeb96629ed91165c6cb130b24d5ff67d6a38f521
magala-richard <rmagala640@gmail.com>
2025-09-24 09:19:13 -0500

    resolve soil profiling

--- apsimNGpy/tests/unittests/test_soil_manager.py
+++ apsimNGpy/tests/unittests/test_soil_manager.py
@@ -27,18 +27,26 @@ class TestSoilManager(unittest.TestCase):
         self.assertIsInstance(sp.cal_missingFromSurgo(), dict)
 
     def test_soil_profile_with_inputs(self):
+        """ensure that organize soil profile raises assertion error if no thickness related values are values are
+        passed into the caller"""
         with self.assertRaises(AssertionError):
             soil = self.soil_download(self.lonlat)
             sp = OrganiseSoilProfile(soil)
             sp.cal_missingFromSurgo()
 
     def test_soil_profile_with_invalid_thickness(self):
+        """Note if thickness is not in mm and max_depth is in mm, there will be a lot of soil layers, a value errror
+        will be raised from soil_layer_calculator"""
         with self.assertRaises(ValueError):
             soil = self.soil_download(self.lonlat)
             sp = OrganiseSoilProfile(soil, thickness=5)
             sp.cal_missingFromSurgo()
 
     def test_soil_profile_calculation_with_seq_thickness_values(self):
+        """
+        At the moment, when the soil profile is calculated, a dict is returned with keys holding to the soil profile section values
+        @return:
+        """
         soil = self.soil_download(self.lonlat)
         sp = OrganiseSoilProfile(soil, thickness_values=self.thickness_values)
         self.assertIsInstance(sp.cal_missingFromSurgo(), dict)

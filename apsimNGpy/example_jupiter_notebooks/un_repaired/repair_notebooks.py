fb501d636cfa1b73982a64d0ff4ab36f141f66fe
magala-richard <rmagala640@gmail.com>
2025-10-05 17:38:26 -0500

    examples

--- apsimNGpy/example_jupiter_notebooks/un_repaired/repair_notebooks.py
+++ apsimNGpy/example_jupiter_notebooks/un_repaired/repair_notebooks.py
@@ -1,3 +1,6 @@
 from apsimNGpy.example_jupiter_notebooks.repair import repair_ipynb
-
-repair_ipynb(in_path='generating_descriptics.ipynb', out_path='../descriptive_stats.ipynb')
+from pathlib import Path
+cwd  = Path(__file__).parent
+notebooks = cwd.glob('**/*.ipynb')
+for i in notebooks:
+   repair_ipynb(in_path=i)

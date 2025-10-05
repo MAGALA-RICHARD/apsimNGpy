5fd8bc4c4736fb31543e383ddc0b2d20cc441717
magala-richard <rmagala640@gmail.com>
2025-10-05 17:36:19 -0500

    examples

--- apsimNGpy/example_jupiter_notebooks/repair_notebooks.py
+++
@@ -1,3 +0,0 @@
-from apsimNGpy.example_jupiter_notebooks.repair import repair_ipynb
-
-repair_ipynb(in_path='generating_descriptics.ipynb', out_path='descriptive_stats.ipynb')
---
+++ apsimNGpy/example_jupiter_notebooks/un_repaired/repair_notebooks.py
@@ -0,0 +1,3 @@
+from apsimNGpy.example_jupiter_notebooks.repair import repair_ipynb
+
+repair_ipynb(in_path='generating_descriptics.ipynb', out_path='../descriptive_stats.ipynb')

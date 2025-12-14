f94b37b6a24fe5e6fe293f1ba41882bea46f68d3
magala-richard <rmagala640@gmail.com>
2025-12-13 22:25:47 -0600

    s└── Models.Core.Simulations: .Simulations     ├── Models.Storage.DataStore: .Simulations.DataStore     └── Models.Sobol: .Simulations.Sobol         └── Models.Core.Simulation: .Simulations.Sobol.Simulation             ├── Models.Clock: .Simulations.Sobol.Simulation.Clock             ├── Models.Core.Zone: .Simulations.Sobol.Simulation.Field             │   ├── Models.Manager: .Simulations.Sobol.Simulation.Field.Fertilise at sowing             │   ├── Models.Fertiliser: .Simulations.Sobol.Simulation.Field.Fertiliser             │   ├── Models.Manager: .Simulations.Sobol.Simulation.Field.Harvest             │   ├── Models.PMF.Plant: .Simulations.Sobol.Simulation.Field.Maize             │   ├── Models.Report: .Simulations.Sobol.Simulation.Field.Report             │   ├── Models.Soils.Soil: .Simulations.Sobol.Simulation.Field.Soil             │   │   ├── Models.Soils.Chemical: .Simulations.Sobol.Simulation.Field.Soil.Chemical             │   │   ├── Models.Soils.Solute: .Simulations.Sobol.Simulation.Field.Soil.NH4             │   │   ├── Models.Soils.Solute: .Simulations.Sobol.Simulation.Field.Soil.NO3             │   │   ├── Models.Soils.Organic: .Simulations.Sobol.Simulation.Field.Soil.Organic             │   │   ├── Models.Soils.Physical: .Simulations.Sobol.Simulation.Field.Soil.Physical             │   │   │   └── Models.Soils.SoilCrop: .Simulations.Sobol.Simulation.Field.Soil.Physical.MaizeSoil             │   │   ├── Models.Soils.Solute: .Simulations.Sobol.Simulation.Field.Soil.Urea             │   │   └── Models.Soils.Water: .Simulations.Sobol.Simulation.Field.Soil.Water             │   ├── Models.Manager: .Simulations.Sobol.Simulation.Field.Sow using a variable rule             │   └── Models.Surface.SurfaceOrganicMatter: .Simulations.Sobol.Simulation.Field.SurfaceOrganicMatter             ├── Models.Graph: .Simulations.Sobol.Simulation.Graph             │   └── Models.Series: .Simulations.Sobol.Simulation.Graph.Series             ├── Models.MicroClimate: .Simulations.Sobol.Simulation.MicroClimate             ├── Models.Soils.Arbitrator.SoilArbitrator: .Simulations.Sobol.Simulation.SoilArbitrator             ├── Models.Summary: .Simulations.Sobol.Simulation.Summary             └── Models.Climate.Weather: .Simulations.Sobol.Simulation.Weather enstivity manger added

--- apsimNGpy/optimizer/minimize/single_mixed.py
+++ apsimNGpy/optimizer/minimize/single_mixed.py
@@ -281,6 +281,14 @@ if __name__ == '__main__':
         # other params must be on the same node or associated or extra arguments, e.g., target simulation name classified simulations
         'cultivar': True
     }
+    cultivar_param_p = {
+        "path": ".Simulations.Simulation.Field.Maize.CultivarFolder.Dekalb_XL82",
+        "start_value": [550, ],
+        "candidate_param": ["[Grain].MaximumGrainsPerCob.FixedValue", ],
+        "other_params": {"sowed": True},
+        # other params must be on the same node or associated or extra arguments, e.g., target simulation name classified simulations
+        'cultivar': True
+    }
 
     # _______________________________________
     # use cases
@@ -298,6 +306,17 @@ if __name__ == '__main__':
     # min.minimize_with_de(workers=3, updating='deferred')
     # minim.minimize_with_alocal_solver(method='Nelder-Mead')
     res = minim.minimize_with_de(use_threads=True, updating='deferred', workers=15, popsize=10, constraints=(0, 0.2))
+    print(res)
+    out = minim.minimize_with_local()
+    print(out)
+    # _____________________
+    # no vtyped variables
+    # _____________________
+    mp = MixedProblem(model='Maize', trainer_dataset=obs, pred_col='Yield', metric='RRMSE', table='Report',
+                      index='year', trainer_col='observed')
+    mp.submit_factor(**cultivar_param_p)
+    print(mp.n_factors, 'factors submitted')
+    res = minim.minimize_with_de(use_threads=False, updating='deferred', workers=15, popsize=10, constraints=(0, 0.2))
     print(res)
     out = minim.minimize_with_local()
     print(out)

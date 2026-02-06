83011deee7545cfe4100e234a6946dec676cef23
magala-richard <rmagala640@gmail.com>
2026-02-05 19:20:37 -0600

    starter module

--- apsimNGpy/tests/unittests/core/model_loader.py
+++ apsimNGpy/tests/unittests/core/model_loader.py
@@ -3,7 +3,7 @@ import os
 import unittest
 from pathlib import Path
 
-from starter.cs_resources import CastHelper as CastHelpers
+from apsimNGpy.starter.cs_resources import CastHelper as CastHelpers
 from apsimNGpy.core.model_loader import (load_apsim_model, get_model, save_model_to_file, load_from_path, load_crop_from_disk, load_from_dict)
 from starter.starter import Models
 from apsimNGpy.exceptions import CastCompilationError

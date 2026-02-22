34fecd4936e3b5886868959aa087b4fb5fec50ba
magala-richard <rmagala640@gmail.com>
2026-02-22 14:33:37 -0600

    Add apsimNGpy namespace objects to apsim_bin_context Class as attributes for a cleaner and safer API

--- apsimNGpy/tests/conig_apsim_bin_context.py
+++ apsimNGpy/tests/conig_apsim_bin_context.py
@@ -1,10 +1,11 @@
 import os
 from apsimNGpy.logger import logger
-from apsimNGpy.config import apsim_bin_context, path_checker
+from apsimNGpy.config import apsim_bin_context, path_checker, configuration, Path, set_apsim_bin_path
 import unittest
 from dotenv import load_dotenv
-
+import time
 BIN_KEY = '7980'
+BIN_KEY2 = '7990'
 ENV_FILE = '../.env'
 
 path_checker(ENV_FILE)
@@ -15,6 +16,19 @@ class TestConfigApsimBinContext(unittest.TestCase):
         load_dotenv()
         bin_path = os.getenv(BIN_KEY)
         self.bin_path = bin_path
+
+    def test_bin_path_with_context(self):
+        # first change it
+        bp = os.getenv(BIN_KEY2)
+        set_apsim_bin_path(bp)
+        bin_context = apsim_bin_context(self.bin_path)
+        eq = Path(self.bin_path) == Path(configuration.bin_path)
+        self.assertTrue(eq)
+
+    def test_bin_path_no_context(self):
+        with apsim_bin_context(self.bin_path):
+            eq = Path(self.bin_path) == Path(configuration.bin_path)
+            self.assertTrue(eq)
 
     def skip(self):
         if not path_checker(self.bin_path):
@@ -44,7 +58,7 @@ class TestConfigApsimBinContext(unittest.TestCase):
         with apsim_bin_context(dotenv_path=ENV_FILE, bin_key=BIN_KEY) as bin_run_time:
             with bin_run_time.ApsimModel('Maize') as model:
                 model.run()
-                print(model.summarize_numeric())
+                self.assertFalse(model.results.empty, 'Results are empty')
 
     def test_apsim_bin_context_direct_path(self):
         """
@@ -55,7 +69,7 @@ class TestConfigApsimBinContext(unittest.TestCase):
         with apsim_bin_context(apsim_bin_path=self.bin_path) as bin_run_time:
             with bin_run_time.ApsimModel('Maize') as model:
                 model.run()
-                print(model.summarize_numeric())
+                self.assertFalse(model.results.empty, 'Results are empty')
 
     def test_apsim_bin_context_no_context(self):
         """
@@ -66,7 +80,50 @@ class TestConfigApsimBinContext(unittest.TestCase):
         bin_run_time = apsim_bin_context(apsim_bin_path=self.bin_path)
         with bin_run_time.ApsimModel('Maize') as model:
             model.run()
-            print(model.summarize_numeric())
+            self.assertFalse(model.results.empty, 'Results are empty')
+
+    def test_attributes_attached_no_context(self):
+        """Ensure required namespace attributes are exposed by apsim_bin_context."""
+        self.skip()
+
+        bin_runtime = apsim_bin_context(self.bin_path)
+
+        # --- Attribute existence checks ---
+        self.assertTrue(hasattr(bin_runtime, "ApsimModel"))
+        self.assertTrue(hasattr(bin_runtime, "MultiCoreManager"))
+        self.assertTrue(hasattr(bin_runtime, "run_apsim_by_path"))
+        self.assertTrue(hasattr(bin_runtime, "sensitivity"))
+        self.assertTrue(hasattr(bin_runtime, "ConfigProblem"))
+        self.assertTrue(hasattr(bin_runtime, "ExperimentManager"))
+
+        # --- Type / callable checks ---
+        self.assertTrue(callable(bin_runtime.ApsimModel))
+        self.assertTrue(callable(bin_runtime.MultiCoreManager))
+        self.assertTrue(callable(bin_runtime.run_apsim_by_path))
+        self.assertTrue(callable(bin_runtime.sensitivity))
+        self.assertTrue(callable(bin_runtime.ConfigProblem))
+        self.assertTrue(callable(bin_runtime.ExperimentManager))
+
+    def test_attributes_attached_with_context(self):
+        """Ensure required namespace attributes are exposed by apsim_bin_context."""
+        self.skip()
+
+        with  apsim_bin_context(self.bin_path) as bin_runtime:
+            # --- Attribute existence checks ---
+            self.assertTrue(hasattr(bin_runtime, "ApsimModel"))
+            self.assertTrue(hasattr(bin_runtime, "MultiCoreManager"))
+            self.assertTrue(hasattr(bin_runtime, "run_apsim_by_path"))
+            self.assertTrue(hasattr(bin_runtime, "sensitivity"))
+            self.assertTrue(hasattr(bin_runtime, "ConfigProblem"))
+            self.assertTrue(hasattr(bin_runtime, "ExperimentManager"))
+
+            # --- Type / callable checks ---
+            self.assertTrue(callable(bin_runtime.ApsimModel))
+            self.assertTrue(callable(bin_runtime.MultiCoreManager))
+            self.assertTrue(callable(bin_runtime.run_apsim_by_path))
+            self.assertTrue(callable(bin_runtime.sensitivity))
+            self.assertTrue(callable(bin_runtime.ConfigProblem))
+            self.assertTrue(callable(bin_runtime.ExperimentManager))
 
 
 if __name__ == '__main__':

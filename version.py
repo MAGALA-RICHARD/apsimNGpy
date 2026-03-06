b0681e229c9ac171d56db73361b257e1498cecaf
magala-richard <rmagala640@gmail.com>
2026-03-05 20:34:36 -0600

    improve lazy imports of .net module

--- version.py
+++ version.py
@@ -1,12 +1,1 @@
 version  =  '1.5.2'
-import apsimNGpy
-apsimNGpy.unittests.senstivitymanager
-apsimNGpy.unittests.experiment
-apsimNGpy.unittests.apsim
-apsimNGpy.unittests.core_edit_model
-apsimNGpy.unittests.edit_model_by_path
-apsimNGpy.unittests.runner
-apsimNGpy.unittests.experiment
-apsimNGpy.unittests.starter
-apsimNGpy.unittests.weathermanager
-apsimNGpy.unittests.soilmanager

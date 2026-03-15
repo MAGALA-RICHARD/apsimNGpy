890eb01c81b42d1e1c94eb4e5e618ddc131e73a0
magala-richard <rmagala640@gmail.com>
2026-03-15 11:20:30 -0500

    added explict cultivar editor class

--- apsimNGpy/core/ce.py
+++ apsimNGpy/core/ce.py
@@ -3,6 +3,7 @@ The role of this module is to provide explicit support for editing cultivars
 """
 from functools import lru_cache
 
+from apsimNGpy import timer
 from apsimNGpy.exceptions import NodeNotFoundError
 from apsimNGpy.core.apsim import ApsimModel, CLR
 from dataclasses import dataclass
@@ -12,7 +13,7 @@ from apsimNGpy.core.model_tools import ModelTools
 
 def _check_cmds(commands):
     if isinstance(commands, dict):
-        raise ValueError(f'expected a list like but got {type(commands)}')
+        raise TypeError(f'expected a list like but got {type(commands)}')
     elif isinstance(commands, str):
         commands = [commands]
     invalid = set()
@@ -42,6 +43,7 @@ class CultivarEditor:
     def __hash__(self):
         return hash((self.plant, self.template_cultivar, self.commands, self.model))
 
+    @lru_cache(maxsize=200)# templates are the same
     def inspect_commands(self):
         try:
             commands = self.model.inspect_model_parameters('Cultivar', self.template_cultivar)
@@ -61,26 +63,36 @@ class CultivarEditor:
         elif manager in self.model.inspect_model('Models.Manager', fullpath=False):
             self.model.edit_model(model_type='Models.Manager', model_name=manager, **{param_name: name})
         else:
-            # manager does not exisit
+            # manager does not exist
             raise NodeNotFoundError(f'manager script `{manager}` is not available i')
 
+    @lru_cache(maxsize=200)  # crucial during optimization
     def add_plant_replacements(self):
-        plants = self.model.inspect_model('Models.PMF.Plant')
-        for plant in plants:
-            if self.plant == plant.split('.')[-1]:
-                plant_path = plant
-                self.model.add_replacements(plant_path)
-                break
-        else:
-            raise ValueError(f'No plants name `{self.plant}` found')
+        self.model.add_crop_replacements()
         pr = self.model.get_crop_replacement(self.plant)
         plant_path = pr.FullPath
         return plant_path
 
     def get_plant_rep(self):
-        return self.model.get_crop_replacement(self.plant)
+        pn = self.model.get_crop_replacement(self.plant)
+        print(pn.Name, pn)
+        return pn
 
-    def get_plant_node(self):
+    def _load_plant_context(self):
+        """
+           Resolve the plant node and validate the template cultivar.
+
+           Returns
+           -------
+           tuple
+               (pp, pn, template_cultivars) where:
+               - pp : str
+                   Path to the plant replacement node.
+               - pn : Models.PMF.Plant
+                   The resolved plant node cast to a PMF Plant.
+               - template_cultivars : list
+                   Available cultivar names for the plant.
+           """
         pp = self.add_plant_replacements()
         plant_node = get_node_by_path(self.model.Simulations, pp)
         pn = CLR.CastHelper.CastAs[CLR.Models.PMF.Plant](plant_node.Model)
@@ -90,7 +102,7 @@ class CultivarEditor:
         return pp, pn, template_cultivars
 
     def _init_edits(self, name):
-        metas = self.get_plant_node()
+        metas = self._load_plant_context()
         path, node, tpc = metas
 
         temps = self.model.inspect_model('Models.PMF.Cultivar', scope=node)
@@ -105,6 +117,9 @@ class CultivarEditor:
         return clt, node
 
     def _format_cmds(self, new_commands):
+        """Return merged commands formatted as key=value strings."""
+        if not isinstance(new_commands, dict):
+            raise TypeError(f'new_commands must be a dict got {type(new_commands)}')
         base_cmd = self.inspect_commands()
         merge_cmds = base_cmd.copy()
         merge_cmds.update(new_commands)  # new_commands take priority
@@ -112,19 +127,40 @@ class CultivarEditor:
         return fmt_cmds
 
     def _update_cmds(self, cmds, name):
+        """Format and apply command updates to the cultivar, then save the model."""
         if not isinstance(cmds, dict):
-            raise ValueError(f'expected a dict but got {type(cmds)}')
+            raise TypeError(f'expected a dict but got {type(cmds)}')
         clt, clt_node = self._init_edits(name)
         cmds = self._format_cmds(cmds)
         clt.Command = cmds
         self.model.save()
 
-    def edit_cmds_by_dicts(self, name, commands):
+    def edit_by_cmd_pairs(self, name, commands):
+        """
+            Update cultivar commands using a dictionary of parameter-value pairs.
+
+            Parameters
+            ----------
+            name : str
+                nw cultivar name to update.
+            commands : dict
+                Mapping of command parameters to their new values.
+            """
         if not isinstance(commands, dict):
-            raise ValueError(f'expected a dict but got {type(commands)}')
+            raise TypeError(f'expected a dict but got {type(commands)}')
         self._update_cmds(cmds=commands, name=name)
 
-    def edit_cmds_by_list_like(self, name, commands):
+    def edit_by_cmd_iterable(self, name, commands):
+        """
+        Update cultivar commands using a dictionary of parameter-value pairs.
+
+        Parameters
+        ----------
+        name : str
+            nw cultivar name to update.
+        commands : Iterable
+            Iterable of command parameters to their new values.
+        """
         cmds = _check_cmds(commands)
         self._update_cmds(cmds=cmds, name=name)
 
@@ -133,12 +169,12 @@ if __name__ == "__main__":
     model = ApsimModel('Maize', 'model.apsimx')
     cc = CultivarEditor(model=model, template_cultivar='B_100', plant='Maize')
     cc.inspect_commands()
-    cc.get_plant_node()
+    cc._load_plant_context()
     p = cc.add_plant_replacements()
-    cc.edit_cmds_by_dicts('B_1002', commands={'[Phenology].Juvenile.Target.FixedValue': 270})
-    cc.edit_cmds_by_list_like('B_1002', commands={'[Phenology].Juvenile.Target.FixedValue=250'})
+    cc.edit_by_cmd_pairs('B_1002', commands={'[Phenology].Juvenile.Target.FixedValue': 270})
+    cc.edit_by_cmd_iterable('B_1002', commands={'[Phenology].Juvenile.Target.FixedValue=250'})
     print(model.inspect_model_parameters('Cultivar', 'B_1002'))
-    cc.get_plant_rep()
+
     cc.attach_cultivar(name='B_1002', manager='Sow using a variable rule', param_name='CultivarName')
 
     # model.open_in_gui()

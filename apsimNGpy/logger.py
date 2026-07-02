3102774db3d0c8e15c42620943049e6bf95cf9da
MAGALA-RICHARD <rmagala640@gmail.com>
2026-07-02 14:46:32 -0500

    changed logger.py

--- apsimNGpy/logger.py
+++ apsimNGpy/logger.py
@@ -2,12 +2,32 @@ from rich.console import Console
 from rich.logging import RichHandler
 import logging
 
-console = Console(force_terminal=True, stderr=True, tab_size=10)
-logging.basicConfig(
-    level="INFO",
-    format="%(message)s",
-    datefmt="[%X]",
-    handlers=[RichHandler(console=console, show_time=True, show_level=True, show_path=False)]
-)
-logger = logging.getLogger("app")
+# console = Console(force_terminal=True, stderr=True, tab_size=10)
+# logging.basicConfig(
+#     level="INFO",
+#     format="%(message)s",
+#     datefmt="[%X]",
+#     handlers=[RichHandler(console=console, show_time=True, show_level=True, show_path=False)]
+# )
+# logger = logging.getLogger("app")
+#
+# from rich.console import Console
+# from rich.logging import RichHandler
+# import logging
 
+console = Console(force_terminal=True, stderr=True, tab_size=10)
+
+logger = logging.getLogger("apsimNGpy")
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+if not logger.handlers:
+    logger.addHandler(
+        RichHandler(
+            console=console,
+            show_time=True,
+            show_level=True,
+            show_path=False,
+            markup=True,
+        )
+    )

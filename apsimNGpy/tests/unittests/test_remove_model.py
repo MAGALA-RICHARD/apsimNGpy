69ef498625471d27220820f3a9df0513417df513
MAGALA-RICHARD <rmagala640@gmail.com>
2026-07-21 12:51:54 -0500

    added remove_model_by_path

--- apsimNGpy/tests/unittests/test_remove_model.py
+++ apsimNGpy/tests/unittests/test_remove_model.py
@@ -10,18 +10,18 @@ class TestRemoveModel(unittest.TestCase):
         self.model_to_remove_by_path = '.Simulations.Simulation.Field.Sow using a variable rule'
         self.path_not_found = "NotFound.Path.Sow using a variable rule"
 
-    def model_not_in(self, model, member, model_type, fullpath):
+    def assert_model_not_in(self, model, member, model_type, fullpath):
         mns = model.inspect_model(model_type=model_type, fullpath=fullpath)
         self.assertNotIn(member, mns)
 
-    def model_in(self, model, member, model_type, fullpath):
+    def assert_model_in(self, model, member, model_type, fullpath):
         mns = model.inspect_model(model_type=model_type, fullpath=fullpath)
         self.assertIn(member, mns)
 
     def test_remove_model(self):
         with ApsimModel('Maize') as model:
             model.remove_model(model_type='Models.Manager', model_name='Sow using a variable rule')
-            self.model_not_in(model, self.model_to_remove, model_type='Models.Manager', fullpath=False)
+            self.assert_model_not_in(model, self.model_to_remove, model_type='Models.Manager', fullpath=False)
 
             model.remove_model(model_type='Models.Manager', model_name='Sow using a variable rule', missing_ok=True,
                                verbose=True)
@@ -41,25 +41,36 @@ class TestRemoveModel(unittest.TestCase):
         # for nested models, we use remove_model_by_path to be precise
         node2 = '.Simulations.sim2.Field.Sow using a variable rule'
         with ApsimModel('Maize') as model:
+            # mock nested simulation
             model.clone_simulation('sim2')
-            print(model.inspect_model('Models.Core.Simulation'))
+            self.assert_model_in(model, 'sim2', model_type='Models.Core.Simulation', fullpath=False)
+            self.assert_model_in(model, 'Simulation', model_type='Models.Core.Simulation', fullpath=False)
             model.remove_model_by_path(path='.Simulations.Simulation.Field.Sow using a variable rule')
-            self.model_not_in(model, '.Simulations.Simulation.Field.Sow using a variable rule',
-                              model_type='Models.Manager', fullpath=True)
+            self.assert_model_not_in(model, '.Simulations.Simulation.Field.Sow using a variable rule',
+                                     model_type='Models.Manager', fullpath=True)
             # test if a similar model of the same name in another simulation is not deleted
-            self.model_in(model, node2,
-                          model_type='Models.Manager', fullpath=True)
+            self.assert_model_in(model, node2,
+                                 model_type='Models.Manager', fullpath=True)
+
+    def test_removing_a_simulation(self):
+        with ApsimModel('Maize') as model:
+            # mock nested simulation
+            model.clone_simulation('sim2')
+            self.assert_model_in(model, 'sim2', model_type='Models.Core.Simulation', fullpath=False)
+            self.assert_model_in(model, 'Simulation', model_type='Models.Core.Simulation', fullpath=False)
+            model.remove_model_by_path(path='.Simulations.Simulation')
+            self.assert_model_not_in(model, 'Simulation', model_type='Models.Core.Simulation', fullpath=False)
 
     def test_remove_model_by_path(self):
         with ApsimModel('Maize') as model:
             model.remove_model_by_path(self.model_to_remove_by_path)
-            self.model_not_in(model, self.model_to_remove_by_path, model_type='Models.Manager', fullpath=True)
+            self.assert_model_not_in(model, self.model_to_remove_by_path, model_type='Models.Manager', fullpath=True)
 
     def test_remove_model_by_path_verbose(self):
         print("testing remove_model by path", file=sys.stderr)
         with ApsimModel('Maize') as model:
             model.remove_model_by_path(self.model_to_remove_by_path, verbose=True)
-            self.model_not_in(model, self.model_to_remove_by_path, model_type='Models.Manager', fullpath=True)
+            self.assert_model_not_in(model, self.model_to_remove_by_path, model_type='Models.Manager', fullpath=True)
 
     def test_remove_model_by_path_missing(self):
         with ApsimModel('Maize') as model:
